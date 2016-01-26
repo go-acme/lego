08516614dd1c62cf479e337e8eea881e472091d5
Simone Carletti <weppos@weppos.net>
2016-01-26 15:09:33 +0100

    DNSimpleProvider: implement Present/CleanUp

--- acme/dns_challenge_dnsimple.go
+++ acme/dns_challenge_dnsimple.go
@@ -3,6 +3,7 @@ package acme
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/weppos/go-dnsimple/dnsimple"
 )
@@ -32,12 +33,100 @@ func NewDNSProviderDNSimple(dnsimpleEmail, dnsimpleApiKey string) (*DNSProviderD
 
 // Present creates a TXT record to fulfil the dns-01 challenge.
 func (c *DNSProviderDNSimple) Present(domain, token, keyAuth string) error {
+	fqdn, value, ttl := DNS01Record(domain, keyAuth)
+
+	zoneID, err := c.getHostedZoneID(domain)
+	if err != nil {
+		return err
+	}
+
+	recordAttributes := c.newTxtRecord(domain, fqdn, value, ttl)
+	_, _, err = c.client.Domains.CreateRecord(zoneID, *recordAttributes)
+	if err != nil {
+		return fmt.Errorf("DNSimple API call failed: %v", err)
+	}
+
 	return nil
 }
 
 // CleanUp removes the TXT record matching the specified parameters.
 func (c *DNSProviderDNSimple) CleanUp(domain, token, keyAuth string) error {
+	fqdn, _, _ := DNS01Record(domain, keyAuth)
+
+	records, err := c.findTxtRecords(domain, fqdn)
+	if err != nil {
+		return err
+	}
+
+	for _, rec := range records {
+		_, err := c.client.Domains.DeleteRecord(rec.DomainId, rec.Id)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+func (c *DNSProviderDNSimple) getHostedZoneID(domain string) (string, error) {
+	domains, _, err := c.client.Domains.List()
+	if err != nil {
+		return "", fmt.Errorf("DNSimple API call failed: %v", err)
+	}
+
+	var hostedDomain dnsimple.Domain
+	for _, d := range domains {
+		if strings.HasSuffix(domain, d.Name) {
+			if len(d.Name) > len(hostedDomain.Name) {
+				hostedDomain = d
+			}
+		}
+	}
+	if hostedDomain.Id == 0 {
+		return "", fmt.Errorf("No matching DNSimple domain found for domain %s", domain)
+	}
+
+	return fmt.Sprintf("%v", hostedDomain.Id), nil
+}
+
+func (c *DNSProviderDNSimple) findTxtRecords(domain, fqdn string) ([]*dnsimple.Record, error) {
+	zoneID, err := c.getHostedZoneID(domain)
+	if err != nil {
+		return nil, err
+	}
+
+	var records []*dnsimple.Record
+	result, _, err := c.client.Domains.ListRecords(zoneID, "", "TXT")
+	if err != nil {
+		return records, fmt.Errorf("DNSimple API call has failed: %v", err)
+	}
+
+	recordName := c.extractRecordName(fqdn, domain)
+	for _, record := range result {
+		if record.Name == recordName {
+			records = append(records, &record)
+		}
+	}
+
+	return records, nil
+}
+
+func (c *DNSProviderDNSimple) newTxtRecord(domain, fqdn, value string, ttl int) *dnsimple.Record {
+	name := c.extractRecordName(fqdn, domain)
+
+	return &dnsimple.Record{
+		Type:    "TXT",
+		Name:    name,
+		Content: value,
+		TTL:     ttl,
+	}
+}
+
+func (c *DNSProviderDNSimple) extractRecordName(fqdn, domain string) string {
+	name := unFqdn(fqdn)
+	if idx := strings.Index(name, "."+domain); idx != -1 {
+		return name[:idx]
+	}
+	return name
 }
 
 func dnsimpleEnvAuth() (email, apiKey string) {

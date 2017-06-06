69c2d230ba95194462a0bc014e9925851d4196d1
Christian Groschupp <christian@groschupp.org>
2017-06-06 18:59:08 +0200

    Added tests for provider otc.

---
+++ providers/dns/otc/otc_test.go
@@ -0,0 +1,62 @@
+package otc
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type OTCDNSTestSuite struct {
+	suite.Suite
+	Mock *DNSMock
+}
+
+func (s *OTCDNSTestSuite) TearDownSuite() {
+	s.Mock.ShutdownServer()
+}
+
+func (s *OTCDNSTestSuite) SetupTest() {
+	s.Mock = NewDNSMock(s.T())
+	s.Mock.Setup()
+	s.Mock.HandleAuthSuccessfully()
+
+}
+
+func TestOTCDNSTestSuite(t *testing.T) {
+	suite.Run(t, new(OTCDNSTestSuite))
+}
+
+func(s *OTCDNSTestSuite) createDNSProvider() (*DNSProvider, error) {
+	url := fmt.Sprintf("%s/v3/auth/token", s.Mock.Server.URL)
+	return NewDNSProviderCredentials(fakeOTCUserName, fakeOTCPassword, fakeOTCDomainName, fakeOTCProjectName, url)
+}
+
+func (s *OTCDNSTestSuite) TestOTCDNSLogin() {
+	otcProvider, err := s.createDNSProvider()
+
+	assert.Nil(s.T(), err)
+	err = otcProvider.LoginRequest()
+	assert.Nil(s.T(), err)
+	assert.Equal(s.T(), otcProvider.otcBaseURL, fmt.Sprintf("%s/v2", s.Mock.Server.URL))
+	assert.Equal(s.T(), fakeOTCToken, otcProvider.token)
+}
+
+func (s *OTCDNSTestSuite) TestOTCDNSPresent() {
+	s.Mock.HandleListZonesSuccessfully()
+	s.Mock.HandleListRecordsetsSuccessfully()
+
+	otcProvider, _ := s.createDNSProvider()
+	err := otcProvider.Present("example.com", "", "foobar")
+	assert.Nil(s.T(), err)
+}
+
+func (s *OTCDNSTestSuite) TestOTCDNSCleanup() {
+	s.Mock.HandleListZonesSuccessfully()
+	s.Mock.HandleListRecordsetsSuccessfully()
+	s.Mock.HandleDeleteRecordsetsSuccessfully()
+
+	otcProvider, _ := s.createDNSProvider()
+	err := otcProvider.CleanUp("example.com", "", "foobar")
+	assert.Nil(s.T(), err)
+}

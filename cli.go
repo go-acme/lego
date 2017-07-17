b2aab0377cb8774886983ffefac5695ebf8aaf3c
James Nugent <james@jen20.com>
2017-07-17 14:50:53 -0500

    dns/route53: Allow specifying hosted zone ID (#345)
    * dns/route53: Allow specifying hosted zone ID
    
    This commit adds support for specifying hosted zone ID via the
    environment variable AWS_HOSTED_ZONE_ID. If this is not specified, the
    previous discovery process is used.
    
    This is useful in environments where multiple hosted zones for the same
    domain name are present in an account.
    
    * dns/route53: Fix up getHostedZoneID method params
    
    Now that getHostedZoneID is a method on the DNSProvider struct, there is
    no reason for it to take the Route53 client as a parameter - we can
    simply use the reference stored in the struct.

--- cli.go
+++ cli.go
@@ -215,7 +215,7 @@ Here is an example bash command using the CloudFlare DNS provider:
 	fmt.Fprintln(w, "\tnamecheap:\tNAMECHEAP_API_USER, NAMECHEAP_API_KEY")
 	fmt.Fprintln(w, "\trackspace:\tRACKSPACE_USER, RACKSPACE_API_KEY")
 	fmt.Fprintln(w, "\trfc2136:\tRFC2136_TSIG_KEY, RFC2136_TSIG_SECRET,\n\t\tRFC2136_TSIG_ALGORITHM, RFC2136_NAMESERVER")
-	fmt.Fprintln(w, "\troute53:\tAWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION")
+	fmt.Fprintln(w, "\troute53:\tAWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, AWS_HOSTED_ZONE_ID")
 	fmt.Fprintln(w, "\tdyn:\tDYN_CUSTOMER_NAME, DYN_USER_NAME, DYN_PASSWORD")
 	fmt.Fprintln(w, "\tvultr:\tVULTR_API_KEY")
 	fmt.Fprintln(w, "\tovh:\tOVH_ENDPOINT, OVH_APPLICATION_KEY, OVH_APPLICATION_SECRET, OVH_CONSUMER_KEY")

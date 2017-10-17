aec5da664ad646a48d87bf166586e3ff12c1d324
Yaroslav Molochko <onorua@gmail.com>
2017-10-18 02:12:29 +0300

    Remove printlns

--- acme/dns_challenge.go
+++ acme/dns_challenge.go
@@ -264,8 +264,6 @@ func FindZoneByFqdn(fqdn string, nameservers []string) (string, error) {
 						zone += i + "."
 					}
 
-					println("Zone", zone)
-					println("Fqdn", fqdn)
 					return zone, nil
 
 				}

1028c3b19077ea5797d7c6b84eafd3b3a500f8d1
Adrian Bjugård <adrian@qng.se>
2018-03-31 16:33:48 +0200

    Add DNS-01 solver using the GleSYS API (#502)
    * Add GleSYS DNS-01 solver
    
    * API url is not overridden during tests
    
    * Use logging package
    
    * Correct documentation for NewDNSProvider

--- cli.go
+++ cli.go
@@ -212,6 +212,7 @@ Here is an example bash command using the CloudFlare DNS provider:
 	fmt.Fprintln(w, "\tgandi:\tGANDI_API_KEY")
 	fmt.Fprintln(w, "\tgandiv5:\tGANDIV5_API_KEY")
 	fmt.Fprintln(w, "\tgcloud:\tGCE_PROJECT, GCE_SERVICE_ACCOUNT_FILE")
+	fmt.Fprintln(w, "\tglesys:\tGLESYS_API_USER, GLESYS_API_KEY")
 	fmt.Fprintln(w, "\tlinode:\tLINODE_API_KEY")
 	fmt.Fprintln(w, "\tlightsail:\tAWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, DNS_ZONE")
 	fmt.Fprintln(w, "\tmanual:\tnone")

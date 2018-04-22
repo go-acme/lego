34f64d2d659be64e287ce1926df2ae812df7234a
Matt Farmer <matt@frmr.me>
2018-04-22 09:28:00 -0400

    Implement acme.GetenvOrFile
    GetenvOrFile will first attempt to read a variable from the environment
    using os.Getenv. Failing that it will see if a variable with "_FILE"
    suffixed on the end exists. If it finds such a variable, it will read
    the referenced file and return its contents as the value.
    
    This functionality will permit those using Docker Swarm to mount their
    secrets (which currently have to come in as files) and then use
    variables like CLOUDFLARE_API_KEY_FILE to tell lego where to find the
    secret.

--- acme/utils.go
+++ acme/utils.go
@@ -2,6 +2,8 @@ package acme
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"time"
 
 	"github.com/xenolf/lego/log"
@@ -30,4 +32,31 @@ func WaitFor(timeout, interval time.Duration, f func() (bool, error)) error {
 
 		time.Sleep(interval)
 	}
+}
+
+// Attempts to resolve 'key' as an environment variable. Failing that, it will
+// check to see if '$key_FILE' exists. If so, it will attempt to read from the
+// referenced file to populate a value.
+func GetenvOrFile(envVar string) string {
+	envVarValue := os.Getenv(envVar)
+
+	if envVarValue != "" {
+		return envVarValue
+	}
+
+	fileVar := envVar + "_FILE"
+	fileVarValue := os.Getenv(fileVar)
+
+	if fileVarValue == "" {
+		return envVarValue
+	}
+
+	fileContents, err := ioutil.ReadFile(fileVarValue)
+
+	if err != nil {
+		fmt.Printf("Error reading the file %s (defined by env var %s): %s\n", fileVarValue, fileVar, err)
+		return ""
+	}
+
+	return string(fileContents)
 }
--- acme/utils_test.go
+++ acme/utils_test.go
@@ -1,6 +1,8 @@
 package acme
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 	"time"
 )
@@ -22,5 +24,37 @@ func TestWaitForTimeout(t *testing.T) {
 		if err == nil {
 			t.Errorf("expected timeout error; got %v", err)
 		}
+	}
+}
+
+func TestGetenvReadsEnvVars(t *testing.T) {
+	os.Setenv("MY_SILLY_ENV_VAR", "bacon")
+	readValue := GetenvOrFile("MY_SILLY_ENV_VAR")
+
+	if readValue != "bacon" {
+		t.Fatal("Expected bacon, got: ", readValue)
+	}
+}
+
+func TestGetenvReadsFiles(t *testing.T) {
+	os.Setenv("MY_SILLY_ENV_VAR_FILE", "/tmp/bacon.env.test")
+	ioutil.WriteFile("/tmp/bacon.env.test", []byte("bacon"), 0644)
+
+	readValue := GetenvOrFile("MY_SILLY_ENV_VAR")
+
+	if readValue != "bacon" {
+		t.Fatal("Expected bacon, got: ", readValue)
+	}
+}
+
+func TestGetenvPrefersEnvVars(t *testing.T) {
+	os.Setenv("MY_SILLY_ENV_VAR", "bacon1")
+	os.Setenv("MY_SILLY_ENV_VAR_FILE", "/tmp/bacon.env.test")
+	ioutil.WriteFile("/tmp/bacon.env.test", []byte("bacon2"), 0644)
+
+	readValue := GetenvOrFile("MY_SILLY_ENV_VAR")
+
+	if readValue != "bacon1" {
+		t.Fatal("Expected bacon1, got: ", readValue)
 	}
 }

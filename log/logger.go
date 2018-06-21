380ff3f270de33a3c28fc1ab9642785d35b83389
Fernandez Ludovic <lfernandez.dev@gmail.com>
2018-06-21 15:46:50 +0200

    refactor: logger init.

--- log/logger.go
+++ log/logger.go
@@ -6,51 +6,35 @@ import (
 )
 
 // Logger is an optional custom logger.
-var Logger *log.Logger
-
-func initDefaultLogger() {
-	if Logger == nil {
-		Logger = log.New(os.Stdout, "", log.LstdFlags)
-	}
-}
+var Logger = log.New(os.Stdout, "", log.LstdFlags)
 
 // Fatal writes a log entry.
 // It uses Logger if not nil, otherwise it uses the default log.Logger.
 func Fatal(args ...interface{}) {
-	initDefaultLogger()
-
 	Logger.Fatal(args...)
 }
 
 // Fatalf writes a log entry.
 // It uses Logger if not nil, otherwise it uses the default log.Logger.
 func Fatalf(format string, args ...interface{}) {
-	initDefaultLogger()
-
 	Logger.Fatalf(format, args...)
 }
 
 // Print writes a log entry.
 // It uses Logger if not nil, otherwise it uses the default log.Logger.
 func Print(args ...interface{}) {
-	initDefaultLogger()
-
 	Logger.Print(args...)
 }
 
 // Println writes a log entry.
 // It uses Logger if not nil, otherwise it uses the default log.Logger.
 func Println(args ...interface{}) {
-	initDefaultLogger()
-
 	Logger.Println(args...)
 }
 
 // Printf writes a log entry.
 // It uses Logger if not nil, otherwise it uses the default log.Logger.
 func Printf(format string, args ...interface{}) {
-	initDefaultLogger()
-
 	Logger.Printf(format, args...)
 }
 

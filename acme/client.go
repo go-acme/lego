9fbc4b365159272ca2f529fb6a9c4376f12405b8
Robert Kästel <robert@kastel.se>
2018-06-25 09:20:37 +0200

    Adjust timeouts.

--- acme/client.go
+++ acme/client.go
@@ -708,9 +708,9 @@ func (c *Client) requestCertificateForCsr(order orderResource, bundle bool, csr 
 		}
 	}
 
-	stopTimer := time.NewTimer(60 * time.Second)
+	stopTimer := time.NewTimer(20 * time.Second)
 	defer stopTimer.Stop()
-	retryTick := time.NewTicker(1000 * time.Millisecond)
+	retryTick := time.NewTicker(200 * time.Millisecond)
 	defer retryTick.Stop()
 
 	for {

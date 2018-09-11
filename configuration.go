088c707d4ce00e413a9a4cba05f845cddef90402
mattn <mattn.jp@gmail.com>
2018-09-12 07:41:30 +0900

    Use path/filepath instead of path (#633)

--- configuration.go
+++ configuration.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/url"
 	"os"
-	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/urfave/cli"
@@ -50,27 +50,26 @@ func (c *Configuration) ExcludedSolvers() (cc []acme.Challenge) {
 // ServerPath returns the OS dependent path to the data for a specific CA
 func (c *Configuration) ServerPath() string {
 	srv, _ := url.Parse(c.context.GlobalString("server"))
-	srvStr := strings.Replace(srv.Host, ":", "_", -1)
-	return strings.Replace(srvStr, "/", string(os.PathSeparator), -1)
+	return strings.NewReplacer(":", "_", "/", string(os.PathSeparator)).Replace(srv.Host)
 }
 
 // CertPath gets the path for certificates.
 func (c *Configuration) CertPath() string {
-	return path.Join(c.context.GlobalString("path"), "certificates")
+	return filepath.Join(c.context.GlobalString("path"), "certificates")
 }
 
 // AccountsPath returns the OS dependent path to the
 // local accounts for a specific CA
 func (c *Configuration) AccountsPath() string {
-	return path.Join(c.context.GlobalString("path"), "accounts", c.ServerPath())
+	return filepath.Join(c.context.GlobalString("path"), "accounts", c.ServerPath())
 }
 
 // AccountPath returns the OS dependent path to a particular account
 func (c *Configuration) AccountPath(acc string) string {
-	return path.Join(c.AccountsPath(), acc)
+	return filepath.Join(c.AccountsPath(), acc)
 }
 
 // AccountKeysPath returns the OS dependent path to the keys of a particular account
 func (c *Configuration) AccountKeysPath(acc string) string {
-	return path.Join(c.AccountPath(acc), "keys")
+	return filepath.Join(c.AccountPath(acc), "keys")
 }

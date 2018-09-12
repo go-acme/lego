b2f27b6cb68b870e47e1fb98ec74bdbfc94775c4
Fernandez Ludovic <lfernandez.dev@gmail.com>
2018-09-13 01:07:49 +0200

    refactor: configuration (pdns).

--- providers/dns/pdns/pdns_test.go
+++ providers/dns/pdns/pdns_test.go
@@ -37,7 +37,12 @@ func TestNewDNSProviderValid(t *testing.T) {
 	os.Setenv("PDNS_API_KEY", "")
 
 	tmpURL, _ := url.Parse("http://localhost:8081")
-	_, err := NewDNSProviderCredentials(tmpURL, "123")
+
+	config := NewDefaultConfig()
+	config.Host = tmpURL
+	config.APIKey = "123"
+
+	_, err := NewDNSProviderConfig(config)
 	assert.NoError(t, err)
 }
 
@@ -56,7 +61,7 @@ func TestNewDNSProviderMissingHostErr(t *testing.T) {
 	os.Setenv("PDNS_API_KEY", "123")
 
 	_, err := NewDNSProvider()
-	assert.EqualError(t, err, "PDNS: some credentials information are missing: PDNS_API_URL")
+	assert.EqualError(t, err, "pdns: some credentials information are missing: PDNS_API_URL")
 }
 
 func TestNewDNSProviderMissingKeyErr(t *testing.T) {
@@ -65,7 +70,7 @@ func TestNewDNSProviderMissingKeyErr(t *testing.T) {
 	os.Setenv("PDNS_API_KEY", "")
 
 	_, err := NewDNSProvider()
-	assert.EqualError(t, err, "PDNS: some credentials information are missing: PDNS_API_KEY,PDNS_API_URL")
+	assert.EqualError(t, err, "pdns: some credentials information are missing: PDNS_API_KEY,PDNS_API_URL")
 }
 
 func TestPdnsPresentAndCleanup(t *testing.T) {
@@ -73,7 +78,11 @@ func TestPdnsPresentAndCleanup(t *testing.T) {
 		t.Skip("skipping live test")
 	}
 
-	provider, err := NewDNSProviderCredentials(pdnsURL, pdnsAPIKey)
+	config := NewDefaultConfig()
+	config.Host = pdnsURL
+	config.APIKey = pdnsAPIKey
+
+	provider, err := NewDNSProviderConfig(config)
 	assert.NoError(t, err)
 
 	err = provider.Present(pdnsDomain, "", "123d==")

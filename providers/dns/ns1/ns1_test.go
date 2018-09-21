621237d07213aa6dead90bdf6fd46251220fa669
Ludovic Fernandez <ldez@users.noreply.github.com>
2018-09-21 17:47:58 +0200

    fix: ns1 auth zone. (#640)

--- providers/dns/ns1/ns1_test.go
+++ providers/dns/ns1/ns1_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var (
@@ -24,6 +25,57 @@ func init() {
 
 func restoreEnv() {
 	os.Setenv("NS1_API_KEY", apiKey)
+}
+
+func Test_getAuthZone(t *testing.T) {
+	type expected struct {
+		AuthZone string
+		Error    string
+	}
+
+	testCases := []struct {
+		desc     string
+		fqdn     string
+		expected expected
+	}{
+		{
+			desc: "valid fqdn",
+			fqdn: "_acme-challenge.myhost.sub.example.com.",
+			expected: expected{
+				AuthZone: "example.com",
+			},
+		},
+		{
+			desc: "invalid fqdn",
+			fqdn: "_acme-challenge.myhost.sub.example.com",
+			expected: expected{
+				Error: "dns: domain must be fully qualified",
+			},
+		},
+		{
+			desc: "invalid authority",
+			fqdn: "_acme-challenge.myhost.sub.domain.tld.",
+			expected: expected{
+				Error: "could not find the start of authority",
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		test := test
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+
+			authZone, err := getAuthZone(test.fqdn)
+
+			if len(test.expected.Error) > 0 {
+				assert.EqualError(t, err, test.expected.Error)
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, test.expected.AuthZone, authZone)
+			}
+		})
+	}
 }
 
 func TestNewDNSProviderValid(t *testing.T) {

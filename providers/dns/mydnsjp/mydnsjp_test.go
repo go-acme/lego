f765be957dd1ed44d741ed3a74a74f4c231f7b5c
Fernandez Ludovic <lfernandez.dev@gmail.com>
2018-09-26 12:23:57 +0200

    review: refactor tests.

--- providers/dns/mydnsjp/mydnsjp_test.go
+++ providers/dns/mydnsjp/mydnsjp_test.go
@@ -30,46 +30,119 @@ func restoreEnv() {
 	os.Setenv("MYDNSJP_PASSWORD", mydnsjpPassword)
 }
 
-func TestNewDNSProviderValid(t *testing.T) {
-	os.Setenv("MYDNSJP_MASTER_ID", "")
-	os.Setenv("MYDNSJP_PASSWORD", "")
-	defer restoreEnv()
+func TestNewDNSProvider(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		envVars  map[string]string
+		expected string
+	}{
+		{
+			desc: "success",
+			envVars: map[string]string{
+				"MYDNSJP_MASTER_ID": "test@example.com",
+				"MYDNSJP_PASSWORD":  "123",
+			},
+		},
+		{
+			desc: "missing credentials",
+			envVars: map[string]string{
+				"MYDNSJP_MASTER_ID": "",
+				"MYDNSJP_PASSWORD":  "",
+			},
+			expected: "mydnsjp: some credentials information are missing: MYDNSJP_MASTER_ID,MYDNSJP_PASSWORD",
+		},
+		{
+			desc: "missing email",
+			envVars: map[string]string{
+				"MYDNSJP_MASTER_ID": "",
+				"MYDNSJP_PASSWORD":  "key",
+			},
+			expected: "mydnsjp: some credentials information are missing: MYDNSJP_MASTER_ID",
+		},
+		{
+			desc: "missing api key",
+			envVars: map[string]string{
+				"MYDNSJP_MASTER_ID": "awesome@possum.com",
+				"MYDNSJP_PASSWORD":  "",
+			},
+			expected: "mydnsjp: some credentials information are missing: MYDNSJP_PASSWORD",
+		},
+	}
 
-	config := NewDefaultConfig()
-	config.MasterID = "123"
-	config.Password = "123"
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			defer restoreEnv()
+			for key, value := range test.envVars {
+				if len(value) == 0 {
+					os.Unsetenv(key)
+				} else {
+					os.Setenv(key, value)
+				}
+			}
 
-	_, err := NewDNSProviderConfig(config)
-	assert.NoError(t, err)
+			p, err := NewDNSProvider()
+
+			if len(test.expected) == 0 {
+				assert.NoError(t, err)
+				assert.NotNil(t, p)
+			} else {
+				require.EqualError(t, err, test.expected)
+			}
+		})
+	}
 }
 
-func TestNewDNSProviderValidEnv(t *testing.T) {
-	defer restoreEnv()
-	os.Setenv("MYDNSJP_MASTER_ID", "test@example.com")
-	os.Setenv("MYDNSJP_PASSWORD", "123")
+func TestNewDNSProviderConfig(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		masterID string
+		password string
+		expected string
+	}{
+		{
+			desc:     "success",
+			masterID: "test@example.com",
+			password: "123",
+		},
+		{
+			desc:     "missing credentials",
+			expected: "mydnsjp: some credentials information are missing",
+		},
+		{
+			desc:     "missing email",
+			password: "123",
+			expected: "mydnsjp: some credentials information are missing",
+		},
+		{
+			desc:     "missing api key",
+			masterID: "test@example.com",
+			expected: "mydnsjp: some credentials information are missing",
+		},
+	}
 
-	_, err := NewDNSProvider()
-	assert.NoError(t, err)
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			defer restoreEnv()
+			os.Unsetenv("MYDNSJP_MASTER_ID")
+			os.Unsetenv("MYDNSJP_PASSWORD")
+
+			config := NewDefaultConfig()
+			config.MasterID = test.masterID
+			config.Password = test.password
+
+			p, err := NewDNSProviderConfig(config)
+
+			if len(test.expected) == 0 {
+				assert.NoError(t, err)
+				assert.NotNil(t, p)
+			} else {
+				require.EqualError(t, err, test.expected)
+			}
+		})
+	}
 }
 
-func TestNewDNSProviderMissingCredErr(t *testing.T) {
-	defer restoreEnv()
-	os.Setenv("MYDNSJP_MASTER_ID", "")
-	os.Setenv("MYDNSJP_PASSWORD", "")
-
-	_, err := NewDNSProvider()
-	assert.EqualError(t, err, "mydnsjp: some credentials information are missing: MYDNSJP_MASTER_ID,MYDNSJP_PASSWORD")
-}
-
-func TestNewDNSProviderMissingCredErrSingle(t *testing.T) {
-	defer restoreEnv()
-	os.Setenv("MYDNSJP_MASTER_ID", "awesome@possum.com")
-
-	_, err := NewDNSProvider()
-	assert.EqualError(t, err, "mydnsjp: some credentials information are missing: MYDNSJP_PASSWORD")
-}
-
-func TestMyDNSJPPresent(t *testing.T) {
+func TestLivePresent(t *testing.T) {
 	if !mydnsjpLiveTest {
 		t.Skip("skipping live test")
 	}
@@ -85,7 +158,7 @@ func TestMyDNSJPPresent(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func TestMyDNSJPCleanUp(t *testing.T) {
+func TestLiveCleanUp(t *testing.T) {
 	if !mydnsjpLiveTest {
 		t.Skip("skipping live test")
 	}

49a6410c2810f0ddd28ef1b5bdd94a6698756e85
Fernandez Ludovic <lfernandez.dev@gmail.com>
2018-10-12 22:22:45 +0200

    refactor(otc): use TestSuite.

--- providers/dns/otc/otc_test.go
+++ providers/dns/otc/otc_test.go
@@ -5,8 +5,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -44,12 +42,12 @@ func (s *TestSuite) createDNSProvider() (*DNSProvider, error) {
 
 func (s *TestSuite) TestLogin() {
 	provider, err := s.createDNSProvider()
-	require.NoError(s.T(), err)
+	s.Require().NoError(err)
 
 	err = provider.loginRequest()
-	require.NoError(s.T(), err)
-	assert.Equal(s.T(), provider.baseURL, fmt.Sprintf("%s/v2", s.Mock.Server.URL))
-	assert.Equal(s.T(), fakeOTCToken, provider.token)
+	s.Require().NoError(err)
+	s.Equal(provider.baseURL, fmt.Sprintf("%s/v2", s.Mock.Server.URL))
+	s.Equal(fakeOTCToken, provider.token)
 }
 
 func (s *TestSuite) TestLoginEnv() {
@@ -62,25 +60,25 @@ func (s *TestSuite) TestLoginEnv() {
 	os.Setenv("OTC_IDENTITY_ENDPOINT", "unittest5")
 
 	provider, err := NewDNSProvider()
-	require.NoError(s.T(), err)
-	assert.Equal(s.T(), provider.config.DomainName, "unittest1")
-	assert.Equal(s.T(), provider.config.UserName, "unittest2")
-	assert.Equal(s.T(), provider.config.Password, "unittest3")
-	assert.Equal(s.T(), provider.config.ProjectName, "unittest4")
-	assert.Equal(s.T(), provider.config.IdentityEndpoint, "unittest5")
+	s.Require().NoError(err)
+	s.Equal(provider.config.DomainName, "unittest1")
+	s.Equal(provider.config.UserName, "unittest2")
+	s.Equal(provider.config.Password, "unittest3")
+	s.Equal(provider.config.ProjectName, "unittest4")
+	s.Equal(provider.config.IdentityEndpoint, "unittest5")
 
 	os.Setenv("OTC_IDENTITY_ENDPOINT", "")
 
 	provider, err = NewDNSProvider()
-	require.NoError(s.T(), err)
-	assert.Equal(s.T(), provider.config.IdentityEndpoint, "https://iam.eu-de.otc.t-systems.com:443/v3/auth/tokens")
+	s.Require().NoError(err)
+	s.Equal(provider.config.IdentityEndpoint, "https://iam.eu-de.otc.t-systems.com:443/v3/auth/tokens")
 }
 
 func (s *TestSuite) TestLoginEnvEmpty() {
 	defer os.Clearenv()
 
 	_, err := NewDNSProvider()
-	assert.EqualError(s.T(), err, "otc: some credentials information are missing: OTC_DOMAIN_NAME,OTC_USER_NAME,OTC_PASSWORD,OTC_PROJECT_NAME")
+	s.EqualError(err, "otc: some credentials information are missing: OTC_DOMAIN_NAME,OTC_USER_NAME,OTC_PASSWORD,OTC_PROJECT_NAME")
 }
 
 func (s *TestSuite) TestDNSProvider_Present() {
@@ -88,10 +86,10 @@ func (s *TestSuite) TestDNSProvider_Present() {
 	s.Mock.HandleListRecordsetsSuccessfully()
 
 	provider, err := s.createDNSProvider()
-	require.NoError(s.T(), err)
+	s.Require().NoError(err)
 
 	err = provider.Present("example.com", "", "foobar")
-	require.NoError(s.T(), err)
+	s.Require().NoError(err)
 }
 
 func (s *TestSuite) TestDNSProvider_Present_EmptyZone() {
@@ -99,10 +97,10 @@ func (s *TestSuite) TestDNSProvider_Present_EmptyZone() {
 	s.Mock.HandleListRecordsetsSuccessfully()
 
 	provider, err := s.createDNSProvider()
-	require.NoError(s.T(), err)
+	s.Require().NoError(err)
 
 	err = provider.Present("example.com", "", "foobar")
-	assert.NotNil(s.T(), err)
+	s.NotNil(err)
 }
 
 func (s *TestSuite) TestDNSProvider_CleanUp() {
@@ -111,10 +109,10 @@ func (s *TestSuite) TestDNSProvider_CleanUp() {
 	s.Mock.HandleDeleteRecordsetsSuccessfully()
 
 	provider, err := s.createDNSProvider()
-	require.NoError(s.T(), err)
+	s.Require().NoError(err)
 
 	err = provider.CleanUp("example.com", "", "foobar")
-	require.NoError(s.T(), err)
+	s.Require().NoError(err)
 }
 
 func (s *TestSuite) TestDNSProvider_CleanUp_EmptyRecordset() {
@@ -122,8 +120,8 @@ func (s *TestSuite) TestDNSProvider_CleanUp_EmptyRecordset() {
 	s.Mock.HandleListRecordsetsEmpty()
 
 	provider, err := s.createDNSProvider()
-	require.NoError(s.T(), err)
+	s.Require().NoError(err)
 
 	err = provider.CleanUp("example.com", "", "foobar")
-	require.Error(s.T(), err)
+	s.Require().Error(err)
 }

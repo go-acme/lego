7663a44ef008baebac146dc46c336b7efc13c0b5
Fernandez Ludovic <lfernandez.dev@gmail.com>
2018-11-03 02:47:55 +0100

    refactor: DNS challenge unit tests.

--- challenge/dns01/dns_challenge_manual_test.go
+++ challenge/dns01/dns_challenge_manual_test.go
@@ -1,10 +1,13 @@
 package dns01
 
 import (
+	"bufio"
 	"crypto/rand"
 	"crypto/rsa"
 	"net/http"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/xenolf/lego/le"
@@ -12,14 +15,16 @@ import (
 	"github.com/xenolf/lego/platform/tester"
 )
 
-type DNSProviderMock struct{}
-
-func (*DNSProviderMock) Present(domain, token, keyAuth string) error { return nil }
-func (*DNSProviderMock) CleanUp(domain, token, keyAuth string) error { return nil }
-
-func TestDNSChallenge(t *testing.T) {
+func TestDNSValidServerResponse(t *testing.T) {
 	_, apiURL, tearDown := tester.SetupFakeAPI()
 	defer tearDown()
+
+	go func() {
+		time.Sleep(time.Second * 2)
+		f := bufio.NewWriter(os.Stdout)
+		defer f.Flush()
+		_, _ = f.WriteString("\n")
+	}()
 
 	privKey, err := rsa.GenerateKey(rand.Reader, 512)
 	require.NoError(t, err)
@@ -27,10 +32,13 @@ func TestDNSChallenge(t *testing.T) {
 	core, err := api.New(http.DefaultClient, "lego-test", apiURL, "", privKey)
 	require.NoError(t, err)
 
+	manualProvider, err := NewDNSProviderManual()
+	require.NoError(t, err)
+
 	validate := func(_ *api.Core, _, _ string, _ le.Challenge) error { return nil }
 	preCheck := func(fqdn, value string) (bool, error) { return true, nil }
 
-	chlg := NewChallenge(core, validate, &DNSProviderMock{}, AddPreCheck(preCheck))
+	chlg := NewChallenge(core, validate, manualProvider, AddPreCheck(preCheck))
 
 	clientChallenge := le.Challenge{Type: "dns01", Status: "pending", URL: apiURL + "/chlg", Token: "http8"}
 

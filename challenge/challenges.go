509d7c5387869620cdae8b916bd71856b8c667d0
Fernandez Ludovic <lfernandez.dev@gmail.com>
2018-11-04 01:13:10 +0100

    refactor: comments.

--- challenge/challenges.go
+++ challenge/challenges.go
@@ -1,15 +1,15 @@
 package challenge
 
-// Type is a string that identifies a particular type and version of ACME challenge.
+// Type is a string that identifies a particular challenge type and version of ACME challenge.
 type Type string
 
 const (
 	// HTTP01 is the "http-01" ACME challenge https://github.com/ietf-wg-acme/acme/blob/master/draft-ietf-acme-acme.md#http
-	// Note: HTTP01ChallengePath returns the URL path to fulfill this challenge
+	// Note: ChallengePath returns the URL path to fulfill this challenge
 	HTTP01 = Type("http-01")
 
 	// DNS01 is the "dns-01" ACME challenge https://github.com/ietf-wg-acme/acme/blob/master/draft-ietf-acme-acme.md#dns
-	// Note: DNS01Record returns a DNS record which will fulfill this challenge
+	// Note: GetRecord returns a DNS record which will fulfill this challenge
 	DNS01 = Type("dns-01")
 
 	// TLSALPN01 is the "tls-alpn-01" ACME challenge https://tools.ietf.org/html/draft-ietf-acme-tls-alpn-01

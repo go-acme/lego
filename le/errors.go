409f251e50b947aeb9efb753d3b3e9fb7e01a946
Fernandez Ludovic <lfernandez.dev@gmail.com>
2018-11-05 20:25:02 +0100

    refactor: add more documentation about ACME objects (from RFC).

--- le/errors.go
+++ le/errors.go
@@ -12,18 +12,39 @@ const (
 
 // ProblemDetails the problem details object
 // - https://tools.ietf.org/html/rfc7807#section-3.1
+// - https://tools.ietf.org/html/draft-ietf-acme-acme-16#section-7.3.3
 type ProblemDetails struct {
-	Type       string `json:"type,omitempty"`
-	Detail     string `json:"detail,omitempty"`
-	HTTPStatus int    `json:"status,omitempty"`
+	Type        string       `json:"type,omitempty"`
+	Detail      string       `json:"detail,omitempty"`
+	HTTPStatus  int          `json:"status,omitempty"`
+	Instance    string       `json:"instance,omitempty"`
+	SubProblems []SubProblem `json:"subproblems,omitempty"`
 
 	// additional values to have a better error message (Not defined by the RFC)
 	Method string `json:"method,omitempty"`
 	URL    string `json:"url,omitempty"`
 }
 
+// SubProblem a "subproblems"
+// - https://tools.ietf.org/html/draft-ietf-acme-acme-16#section-6.7.1
+type SubProblem struct {
+	Type       string     `json:"type,omitempty"`
+	Detail     string     `json:"detail,omitempty"`
+	Identifier Identifier `json:"identifier,omitempty"`
+}
+
 func (p ProblemDetails) Error() string {
-	return fmt.Sprintf("LE error: %d :: %s :: %s :: %s :: %s", p.HTTPStatus, p.Method, p.URL, p.Type, p.Detail)
+	msg := fmt.Sprintf("acme: error: %d :: %s :: %s :: %s :: %s", p.HTTPStatus, p.Method, p.URL, p.Type, p.Detail)
+
+	for _, sub := range p.SubProblems {
+		msg += fmt.Sprintf(", problem: %q :: %s", sub.Type, sub.Detail)
+	}
+
+	if len(p.Instance) == 0 {
+		msg += ", url: " + p.Instance
+	}
+
+	return msg
 }
 
 // NonceError represents the error which is returned

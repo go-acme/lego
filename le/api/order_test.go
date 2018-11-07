1c4b0f1ecbfb155f32d029fa672e0b39e036e72f
Fernandez Ludovic <lfernandez.dev@gmail.com>
2018-11-07 14:54:32 +0100

    refactor: move tests.

---
+++ le/api/order_test.go
@@ -0,0 +1,101 @@
+package api
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"gopkg.in/square/go-jose.v2"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/require"
+	"github.com/xenolf/lego/le"
+	"github.com/xenolf/lego/platform/tester"
+)
+
+func TestOrderService_New(t *testing.T) {
+	mux, apiURL, tearDown := tester.SetupFakeAPI()
+	defer tearDown()
+
+	// small value keeps test fast
+	privKey, err := rsa.GenerateKey(rand.Reader, 512)
+	require.NoError(t, err, "Could not generate test key")
+
+	mux.HandleFunc("/newOrder", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+			return
+		}
+
+		body, err := readSignedBody(r, privKey)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+
+		order := le.Order{}
+		err = json.Unmarshal(body, &order)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		err = tester.WriteJSONResponse(w, le.Order{
+			Status:      le.StatusValid,
+			Identifiers: order.Identifiers,
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	mux.HandleFunc("/nonce", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodHead {
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.Header().Add("Replay-Nonce", "12345")
+		w.Header().Add("Retry-After", "0")
+	})
+
+	core, err := New(http.DefaultClient, "lego-test", apiURL, "", privKey)
+	require.NoError(t, err)
+
+	order, err := core.Orders.New([]string{"example.com"})
+	require.NoError(t, err)
+
+	expected := le.ExtendedOrder{
+		Order: le.Order{
+			Status:      "valid",
+			Identifiers: []le.Identifier{{Type: "dns", Value: "example.com"}},
+		},
+	}
+	assert.Equal(t, expected, order)
+}
+
+func readSignedBody(r *http.Request, privKey *rsa.PrivateKey) ([]byte, error) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	jws, err := jose.ParseSigned(string(reqBody))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := jws.Verify(&jose.JSONWebKey{
+		Key:       privKey.Public(),
+		Algorithm: "RSA",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}

f279698935fd797df276467dd7bca110ae85d46b
Fernandez Ludovic <lfernandez.dev@gmail.com>
2018-12-07 00:38:28 +0100

    fix: missing version.

--- acme/api/internal/sender/useragent.go
+++ acme/api/internal/sender/useragent.go
@@ -2,7 +2,7 @@ package sender
 
 const (
 	// ourUserAgent is the User-Agent of this underlying library package.
-	ourUserAgent = "xenolf-acme"
+	ourUserAgent = "xenolf-acme/1.2.1"
 
 	// ourUserAgentComment is part of the UA comment linked to the version status of this underlying library package.
 	// values: detach|release

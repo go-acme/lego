7193a2c944deff70b46fc8dc6d02137d0245f5b5
Fernandez Ludovic <lfernandez.dev@gmail.com>
2019-02-08 11:45:58 +0100

    fix: CLI and key type.

--- cmd/flags.go
+++ cmd/flags.go
@@ -42,7 +42,7 @@ func CreateFlags(defaultPath string) []cli.Flag {
 		},
 		cli.StringFlag{
 			Name:  "key-type, k",
-			Value: "rsa2048",
+			Value: "ec384",
 			Usage: "Key type to use for private keys. Supported: rsa2048, rsa4096, rsa8192, ec256, ec384.",
 		},
 		cli.StringFlag{

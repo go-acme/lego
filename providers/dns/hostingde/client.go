a1448008967b5f09de3b4737ff6fede38a208cdc
jkahrs <kahrs@platformadmin.de>
2019-02-26 15:25:34 +0100

    hostingde: fix client fails if customer has no access to dns-groups (#809)

--- providers/dns/hostingde/client.go
+++ providers/dns/hostingde/client.go
@@ -2,112 +2,102 @@ package hostingde
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/cenkalti/backoff"
 )
 
 const defaultBaseURL = "https://secure.hosting.de/api/dns/v1/json"
 
-// RecordsAddRequest represents a DNS record to add
-type RecordsAddRequest struct {
-	Name    string `json:"name"`
-	Type    string `json:"type"`
-	Content string `json:"content"`
-	TTL     int    `json:"ttl"`
-}
+// https://www.hosting.de/api/?json#list-zoneconfigs
+func (d *DNSProvider) listZoneConfigs(findRequest ZoneConfigsFindRequest) (*ZoneConfigsFindResponse, error) {
+	uri := defaultBaseURL + "/zoneConfigsFind"
 
-// RecordsDeleteRequest represents a DNS record to remove
-type RecordsDeleteRequest struct {
-	Name    string `json:"name"`
-	Type    string `json:"type"`
-	Content string `json:"content"`
-	ID      string `json:"id"`
-}
+	findResponse := &ZoneConfigsFindResponse{}
 
-// ZoneConfigObject represents the ZoneConfig-section of a hosting.de API response.
-type ZoneConfigObject struct {
-	AccountID      string `json:"accountId"`
-	EmailAddress   string `json:"emailAddress"`
-	ID             string `json:"id"`
-	LastChangeDate string `json:"lastChangeDate"`
-	MasterIP       string `json:"masterIp"`
-	Name           string `json:"name"`
-	NameUnicode    string `json:"nameUnicode"`
-	SOAValues      struct {
-		Expire      int    `json:"expire"`
-		NegativeTTL int    `json:"negativeTtl"`
-		Refresh     int    `json:"refresh"`
-		Retry       int    `json:"retry"`
-		Serial      string `json:"serial"`
-		TTL         int    `json:"ttl"`
-	} `json:"soaValues"`
-	Status                string   `json:"status"`
-	TemplateValues        string   `json:"templateValues"`
-	Type                  string   `json:"type"`
-	ZoneTransferWhitelist []string `json:"zoneTransferWhitelist"`
-}
-
-// ZoneUpdateError represents an error in a ZoneUpdateResponse
-type ZoneUpdateError struct {
-	Code          int      `json:"code"`
-	ContextObject string   `json:"contextObject"`
-	ContextPath   string   `json:"contextPath"`
-	Details       []string `json:"details"`
-	Text          string   `json:"text"`
-	Value         string   `json:"value"`
-}
-
-// ZoneUpdateMetadata represents the metadata in a ZoneUpdateResponse
-type ZoneUpdateMetadata struct {
-	ClientTransactionID string `json:"clientTransactionId"`
-	ServerTransactionID string `json:"serverTransactionId"`
-}
-
-// ZoneUpdateResponse represents a response from hosting.de API
-type ZoneUpdateResponse struct {
-	Errors   []ZoneUpdateError  `json:"errors"`
-	Metadata ZoneUpdateMetadata `json:"metadata"`
-	Warnings []string           `json:"warnings"`
-	Status   string             `json:"status"`
-	Response struct {
-		Records []struct {
-			Content          string `json:"content"`
-			Type             string `json:"type"`
-			ID               string `json:"id"`
-			Name             string `json:"name"`
-			LastChangeDate   string `json:"lastChangeDate"`
-			Priority         int    `json:"priority"`
-			RecordTemplateID string `json:"recordTemplateId"`
-			ZoneConfigID     string `json:"zoneConfigId"`
-			TTL              int    `json:"ttl"`
-		} `json:"records"`
-		ZoneConfig ZoneConfigObject `json:"zoneConfig"`
-	} `json:"response"`
-}
-
-// ZoneConfigSelector represents a "minimal" ZoneConfig object used in hosting.de API requests
-type ZoneConfigSelector struct {
-	Name string `json:"name"`
-}
-
-// ZoneUpdateRequest represents a hosting.de API ZoneUpdate request
-type ZoneUpdateRequest struct {
-	AuthToken          string `json:"authToken"`
-	ZoneConfigSelector `json:"zoneConfig"`
-	RecordsToAdd       []RecordsAddRequest    `json:"recordsToAdd"`
-	RecordsToDelete    []RecordsDeleteRequest `json:"recordsToDelete"`
-}
-
-func (d *DNSProvider) updateZone(updateRequest ZoneUpdateRequest) (*ZoneUpdateResponse, error) {
-	body, err := json.Marshal(updateRequest)
+	rawResp, err := d.post(uri, findRequest, findResponse)
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := http.NewRequest(http.MethodPost, defaultBaseURL+"/zoneUpdate", bytes.NewReader(body))
+	if len(findResponse.Response.Data) == 0 {
+		return nil, fmt.Errorf("%v: %s", err, toUnreadableBodyMessage(uri, rawResp))
+	}
+
+	if findResponse.Status != "success" && findResponse.Status != "pending" {
+		return findResponse, errors.New(toUnreadableBodyMessage(uri, rawResp))
+	}
+
+	return findResponse, nil
+}
+
+// https://www.hosting.de/api/?json#updating-zones
+func (d *DNSProvider) updateZone(updateRequest ZoneUpdateRequest) (*ZoneUpdateResponse, error) {
+	uri := defaultBaseURL + "/zoneUpdate"
+
+	// but we'll need the ID later to delete the record
+	updateResponse := &ZoneUpdateResponse{}
+
+	rawResp, err := d.post(uri, updateRequest, updateResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	if updateResponse.Status != "success" && updateResponse.Status != "pending" {
+		return nil, errors.New(toUnreadableBodyMessage(uri, rawResp))
+	}
+
+	return updateResponse, nil
+}
+
+func (d *DNSProvider) getZone(findRequest ZoneConfigsFindRequest) (*ZoneConfig, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	var zoneConfig *ZoneConfig
+
+	operation := func() error {
+		findResponse, err := d.listZoneConfigs(findRequest)
+		if err != nil {
+			cancel()
+			return err
+		}
+
+		if findResponse.Response.Data[0].Status != "active" {
+			return fmt.Errorf("unexpected status: %q", findResponse.Response.Data[0].Status)
+		}
+
+		zoneConfig = &findResponse.Response.Data[0]
+
+		return nil
+	}
+
+	bo := backoff.NewExponentialBackOff()
+	bo.InitialInterval = 3 * time.Second
+	bo.MaxInterval = 10 * bo.InitialInterval
+	bo.MaxElapsedTime = 100 * bo.InitialInterval
+
+	// retry in case the zone was edited recently and is not yet active
+	err := backoff.Retry(operation, backoff.WithContext(bo, ctx))
+	if err != nil {
+		return nil, err
+	}
+
+	return zoneConfig, nil
+}
+
+func (d *DNSProvider) post(uri string, request interface{}, response interface{}) ([]byte, error) {
+	body, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, uri, bytes.NewReader(body))
 	if err != nil {
 		return nil, err
 	}
@@ -121,23 +111,17 @@ func (d *DNSProvider) updateZone(updateRequest ZoneUpdateRequest) (*ZoneUpdateRe
 
 	content, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, errors.New(toUnreadableBodyMessage(req, content))
+		return nil, errors.New(toUnreadableBodyMessage(uri, content))
 	}
 
-	// Everything looks good; but we'll need the ID later to delete the record
-	updateResponse := &ZoneUpdateResponse{}
-	err = json.Unmarshal(content, updateResponse)
+	err = json.Unmarshal(content, response)
 	if err != nil {
-		return nil, fmt.Errorf("%v: %s", err, toUnreadableBodyMessage(req, content))
+		return nil, fmt.Errorf("%v: %s", err, toUnreadableBodyMessage(uri, content))
 	}
 
-	if updateResponse.Status != "success" && updateResponse.Status != "pending" {
-		return updateResponse, errors.New(toUnreadableBodyMessage(req, content))
-	}
-
-	return updateResponse, nil
+	return content, nil
 }
 
-func toUnreadableBodyMessage(req *http.Request, rawBody []byte) string {
-	return fmt.Sprintf("the request %s sent a response with a body which is an invalid format: %q", req.URL, string(rawBody))
+func toUnreadableBodyMessage(uri string, rawBody []byte) string {
+	return fmt.Sprintf("the request %s sent a response with a body which is an invalid format: %q", uri, string(rawBody))
 }

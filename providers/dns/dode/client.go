d0ee8f2b9e0fc7ab24c68c0a681d9563bee5a214
andig <cpuidle@gmx.de>
2019-03-20 12:00:54 +0100

    Add Domain Offensive (do.de) provider

---
+++ providers/dns/dode/client.go
@@ -0,0 +1,58 @@
+package dode
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+)
+
+type apiResponse struct {
+	Domain  string
+	Success bool
+}
+
+// updateTxtRecord Update the domains TXT record
+// To update the TXT record we just need to make one simple get request.
+func (d *DNSProvider) updateTxtRecord(domain, token, txt string, clear bool) error {
+	u, _ := url.Parse("https://www.do.de/api/letsencrypt")
+
+	// api only accepts domains with challenge prefix
+	domain = "_acme-challenge." + domain
+
+	query := u.Query()
+	query.Set("token", token)
+	query.Set("domain", domain)
+
+	// api call differs per set/delete
+	if clear {
+		query.Set("action", "delete")
+	} else {
+		query.Set("value", txt)
+	}
+
+	u.RawQuery = query.Encode()
+
+	response, err := d.config.HTTPClient.Get(u.String())
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	var r apiResponse
+	err = json.Unmarshal(bodyBytes, &r)
+	if err != nil {
+		return fmt.Errorf("request to change TXT record for do.de returned the following invalid json (%s); used url [%s]", string(bodyBytes), u)
+	}
+
+	body := string(bodyBytes)
+	if !r.Success {
+		return fmt.Errorf("request to change TXT record for do.de returned the following error result (%s); used url [%s]", body, u)
+	}
+	return nil
+}

59f2e46c3476b877ffe5d8c8484eab796938d433
Fernandez Ludovic <lfernandez.dev@gmail.com>
2019-04-28 14:25:12 +0200

    review: isolate client code.

---
+++ providers/dns/joker/client_test.go
@@ -0,0 +1,447 @@
+package joker
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	correctAuth     = "123"
+	incorrectAuth   = "321"
+	serverErrorAuth = "500"
+)
+
+func setup() (*http.ServeMux, *httptest.Server) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	return mux, server
+}
+
+func TestDNSProvider_login(t *testing.T) {
+	testCases := []struct {
+		desc               string
+		authKey            string
+		expectedError      bool
+		expectedStatusCode int
+		expectedAuthSid    string
+	}{
+		{
+			desc:               "correct key",
+			authKey:            correctAuth,
+			expectedStatusCode: 0,
+			expectedAuthSid:    correctAuth,
+		},
+		{
+			desc:               "incorrect key",
+			authKey:            incorrectAuth,
+			expectedStatusCode: 2200,
+			expectedError:      true,
+		},
+		{
+			desc:               "server error",
+			authKey:            serverErrorAuth,
+			expectedStatusCode: -500,
+			expectedError:      true,
+		},
+		{
+			desc:               "non-ok status code",
+			authKey:            "333",
+			expectedStatusCode: 2202,
+			expectedError:      true,
+		},
+	}
+
+	mux, server := setup()
+	defer server.Close()
+
+	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, "POST", r.Method)
+
+		switch r.FormValue("api-key") {
+		case correctAuth:
+			_, _ = io.WriteString(w, "Status-Code: 0\nStatus-Text: OK\nAuth-Sid: 123\n\ncom\nnet")
+		case incorrectAuth:
+			_, _ = io.WriteString(w, "Status-Code: 2200\nStatus-Text: Authentication error")
+		case serverErrorAuth:
+			http.NotFound(w, r)
+		default:
+			_, _ = io.WriteString(w, "Status-Code: 2202\nStatus-Text: OK\n\ncom\nnet")
+		}
+	})
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			config := NewDefaultConfig()
+			config.BaseURL = server.URL
+			config.APIKey = test.authKey
+
+			p, err := NewDNSProviderConfig(config)
+			require.NoError(t, err)
+			require.NotNil(t, p)
+
+			response, err := p.login()
+			if test.expectedError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.NotNil(t, response)
+				assert.Equal(t, test.expectedStatusCode, response.StatusCode)
+				assert.Equal(t, test.expectedAuthSid, response.AuthSid)
+			}
+		})
+	}
+}
+
+func TestDNSProvider_logout(t *testing.T) {
+	testCases := []struct {
+		desc               string
+		authSid            string
+		expectedError      bool
+		expectedStatusCode int
+	}{
+		{
+			desc:               "correct auth-sid",
+			authSid:            correctAuth,
+			expectedStatusCode: 0,
+		},
+		{
+			desc:               "incorrect auth-sid",
+			authSid:            incorrectAuth,
+			expectedStatusCode: 2200,
+		},
+		{
+			desc:          "already logged out",
+			authSid:       "",
+			expectedError: true,
+		},
+		{
+			desc:          "server error",
+			authSid:       serverErrorAuth,
+			expectedError: true,
+		},
+	}
+
+	mux, server := setup()
+	defer server.Close()
+
+	mux.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, "POST", r.Method)
+
+		switch r.FormValue("auth-sid") {
+		case correctAuth:
+			_, _ = io.WriteString(w, "Status-Code: 0\nStatus-Text: OK\n")
+		case incorrectAuth:
+			_, _ = io.WriteString(w, "Status-Code: 2200\nStatus-Text: Authentication error")
+		default:
+			http.NotFound(w, r)
+		}
+	})
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			config := NewDefaultConfig()
+			config.BaseURL = server.URL
+			config.APIKey = "12345"
+			config.AuthSid = test.authSid
+
+			p, err := NewDNSProviderConfig(config)
+			require.NoError(t, err)
+			require.NotNil(t, p)
+
+			response, err := p.logout()
+			if test.expectedError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.NotNil(t, response)
+				assert.Equal(t, test.expectedStatusCode, response.StatusCode)
+			}
+		})
+	}
+}
+
+func TestDNSProvider_getZone(t *testing.T) {
+	var testZone = "@ A 0 192.0.2.2 3600"
+
+	testCases := []struct {
+		desc               string
+		authSid            string
+		domain             string
+		zone               string
+		expectedError      bool
+		expectedStatusCode int
+	}{
+		{
+			desc:               "correct auth-sid, known domain",
+			authSid:            correctAuth,
+			domain:             "known",
+			zone:               testZone,
+			expectedStatusCode: 0,
+		},
+		{
+			desc:               "incorrect auth-sid, known domain",
+			authSid:            incorrectAuth,
+			domain:             "known",
+			expectedStatusCode: 2202,
+		},
+		{
+			desc:               "correct auth-sid, unknown domain",
+			authSid:            correctAuth,
+			domain:             "unknown",
+			expectedStatusCode: 2202,
+		},
+		{
+			desc:          "server error",
+			authSid:       "500",
+			expectedError: true,
+		},
+	}
+
+	mux, server := setup()
+	defer server.Close()
+
+	mux.HandleFunc("/dns-zone-get", func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, "POST", r.Method)
+
+		authSid := r.FormValue("auth-sid")
+		domain := r.FormValue("domain")
+
+		switch {
+		case authSid == correctAuth && domain == "known":
+			_, _ = io.WriteString(w, "Status-Code: 0\nStatus-Text: OK\n\n"+testZone)
+		case authSid == incorrectAuth || (authSid == correctAuth && domain == "unknown"):
+			_, _ = io.WriteString(w, "Status-Code: 2202\nStatus-Text: Authorization error")
+		default:
+			http.NotFound(w, r)
+		}
+	})
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			config := NewDefaultConfig()
+			config.BaseURL = server.URL
+			config.APIKey = "12345"
+			config.AuthSid = test.authSid
+
+			p, err := NewDNSProviderConfig(config)
+			require.NoError(t, err)
+			require.NotNil(t, p)
+
+			response, err := p.getZone(test.domain)
+			if test.expectedError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.NotNil(t, response)
+				assert.Equal(t, test.expectedStatusCode, response.StatusCode)
+				assert.Equal(t, test.zone, response.Body)
+			}
+		})
+	}
+}
+
+func Test_parseResponse(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		input    string
+		expected *response
+	}{
+		{
+			desc:  "Empty response",
+			input: "",
+			expected: &response{
+				Headers:    url.Values{},
+				StatusCode: -1,
+			},
+		},
+		{
+			desc:  "No headers, just body",
+			input: "\n\nTest body",
+			expected: &response{
+				Headers:    url.Values{},
+				Body:       "Test body",
+				StatusCode: -1,
+			},
+		},
+		{
+			desc:  "Headers and body",
+			input: "Test-Header: value\n\nTest body",
+			expected: &response{
+				Headers:    url.Values{"Test-Header": {"value"}},
+				Body:       "Test body",
+				StatusCode: -1,
+			},
+		},
+		{
+			desc:  "Headers and body + Auth-Sid",
+			input: "Test-Header: value\nAuth-Sid: 123\n\nTest body",
+			expected: &response{
+				Headers:    url.Values{"Test-Header": {"value"}, "Auth-Sid": {"123"}},
+				Body:       "Test body",
+				StatusCode: -1,
+				AuthSid:    "123",
+			},
+		},
+		{
+			desc:  "Headers and body + Status-Text",
+			input: "Test-Header: value\nStatus-Text: OK\n\nTest body",
+			expected: &response{
+				Headers:    url.Values{"Test-Header": {"value"}, "Status-Text": {"OK"}},
+				Body:       "Test body",
+				StatusText: "OK",
+				StatusCode: -1,
+			},
+		},
+		{
+			desc:  "Headers and body + Status-Code",
+			input: "Test-Header: value\nStatus-Code: 2020\n\nTest body",
+			expected: &response{
+				Headers:    url.Values{"Test-Header": {"value"}, "Status-Code": {"2020"}},
+				Body:       "Test body",
+				StatusCode: 2020,
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		test := test
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+
+			response := parseResponse(test.input)
+
+			assert.Equal(t, test.expected, response)
+		})
+	}
+}
+
+func Test_removeTxtEntryFromZone(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		input    string
+		expected string
+		modified bool
+	}{
+		{
+			desc:     "empty zone",
+			input:    "",
+			expected: "",
+			modified: false,
+		},
+		{
+			desc:     "zone with only A entry",
+			input:    "@ A 0 192.0.2.2 3600",
+			expected: "@ A 0 192.0.2.2 3600",
+			modified: false,
+		},
+		{
+			desc:     "zone with only clenup entry",
+			input:    "_acme-challenge TXT 0  \"old \" 120",
+			expected: "",
+			modified: true,
+		},
+		{
+			desc:     "zone with one A and one cleanup entries",
+			input:    "@ A 0 192.0.2.2 3600\n_acme-challenge TXT 0  \"old \" 120",
+			expected: "@ A 0 192.0.2.2 3600",
+			modified: true,
+		},
+		{
+			desc:     "zone with one A and multiple cleanup entries",
+			input:    "@ A 0 192.0.2.2 3600\n_acme-challenge TXT 0  \"old \" 120\n_acme-challenge TXT 0  \"another \" 120",
+			expected: "@ A 0 192.0.2.2 3600",
+			modified: true,
+		},
+	}
+
+	for _, test := range testCases {
+		test := test
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+
+			zone, modified := removeTxtEntryFromZone(test.input, "_acme-challenge")
+			assert.Equal(t, zone, test.expected)
+			assert.Equal(t, modified, test.modified)
+		})
+	}
+}
+
+func Test_addTxtEntryToZone(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		input    string
+		expected string
+	}{
+		{
+			desc:     "empty zone",
+			input:    "",
+			expected: "_acme-challenge TXT 0 \"test\" 120",
+		},
+		{
+			desc:     "zone with A entry",
+			input:    "@ A 0 192.0.2.2 3600",
+			expected: "@ A 0 192.0.2.2 3600\n_acme-challenge TXT 0 \"test\" 120",
+		},
+		{
+			desc:     "zone with required cleanup entry",
+			input:    "_acme-challenge TXT 0  \"old \" 120",
+			expected: "_acme-challenge TXT 0 \"old\" 120\n_acme-challenge TXT 0 \"test\" 120",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+
+			zone := addTxtEntryToZone(test.input, "_acme-challenge", "test", 120)
+			assert.Equal(t, zone, test.expected)
+		})
+	}
+}
+
+func Test_fixTxtLines(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		input    string
+		expected string
+	}{
+		{
+			desc:     "clean-up",
+			input:    `_acme-challenge TXT 0  "SrqD25Gpm3WtIGKCqhgsLeXWE_FAD5Hv9CRoLAHxlIE " 120`,
+			expected: `_acme-challenge TXT 0 "SrqD25Gpm3WtIGKCqhgsLeXWE_FAD5Hv9CRoLAHxlIE" 120`,
+		},
+		{
+			desc:     "already cleaned",
+			input:    `_acme-challenge TXT 0 "SrqD25Gpm3WtIGKCqhgsLeXWE_FAD5Hv9CRoLAHxlIE" 120`,
+			expected: `_acme-challenge TXT 0 "SrqD25Gpm3WtIGKCqhgsLeXWE_FAD5Hv9CRoLAHxlIE" 120`,
+		},
+		{
+			desc:     "special DNS entry",
+			input:    "$dyndns=yes:username:password",
+			expected: "$dyndns=yes:username:password",
+		},
+		{
+			desc:     "SRV entry",
+			input:    "_jabber._tcp SRV 20/0 xmpp-server1.l.google.com:5269 300",
+			expected: "_jabber._tcp SRV 20/0 xmpp-server1.l.google.com:5269 300",
+		},
+		{
+			desc:     "MX entry",
+			input:    "@ MX 10 ASPMX.L.GOOGLE.COM 300",
+			expected: "@ MX 10 ASPMX.L.GOOGLE.COM 300",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+
+			line := fixTxtLines(test.input)
+			assert.Equal(t, line, test.expected)
+		})
+	}
+}
--- providers/dns/joker/joker.go
+++ providers/dns/joker/joker.go
@@ -4,12 +4,8 @@ package joker
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"strconv"
-	"strings"
-
 	"net/http"
-	"net/url"
+	"strings"
 	"time"
 
 	"github.com/go-acme/lego/challenge/dns01"
@@ -17,11 +13,7 @@ import (
 	"github.com/go-acme/lego/platform/config/env"
 )
 
-const (
-	defaultBaseURL = "https://dmapi.joker.com/request/"
-)
-
-// Config is used to configure the creation of the DNSProvider
+// Config is used to configure the creation of the DNSProvider.
 type Config struct {
 	Debug              bool
 	BaseURL            string
@@ -91,28 +83,32 @@ func (d *DNSProvider) Timeout() (timeout, interval time.Duration) {
 
 // Present installs a TXT record for the DNS challenge.
 func (d *DNSProvider) Present(domain, token, keyAuth string) error {
+	fqdn, value := dns01.GetRecord(domain, keyAuth)
 
-	zone, relative, value, err := getRecordNames(domain, keyAuth)
+	zone, err := dns01.FindZoneByFqdn(fqdn)
 	if err != nil {
 		return fmt.Errorf("joker: %v", err)
 	}
+
+	relative := getRelative(fqdn, zone)
+
 	if d.config.Debug {
 		log.Infof("[%s] joker: adding TXT record %q to zone %q with value %q", domain, relative, zone, value)
 	}
 
-	response, err := d.jokerLogin()
+	response, err := d.login()
 	if err != nil {
 		return formatResponseError(response, err)
 	}
 
-	response, err = d.jokerGetZone(zone)
+	response, err = d.getZone(zone)
 	if err != nil || response.StatusCode != 0 {
 		return formatResponseError(response, err)
 	}
 
 	dnsZone := addTxtEntryToZone(response.Body, relative, value, d.config.TTL)
 
-	response, err = d.jokerPutZone(zone, dnsZone)
+	response, err = d.putZone(zone, dnsZone)
 	if err != nil || response.StatusCode != 0 {
 		return formatResponseError(response, err)
 	}
@@ -122,228 +118,57 @@ func (d *DNSProvider) Present(domain, token, keyAuth string) error {
 
 // CleanUp removes a TXT record used for a previous DNS challenge.
 func (d *DNSProvider) CleanUp(domain, token, keyAuth string) error {
-	zone, relative, _, err := getRecordNames(domain, keyAuth)
+	fqdn, _ := dns01.GetRecord(domain, keyAuth)
+
+	zone, err := dns01.FindZoneByFqdn(fqdn)
 	if err != nil {
 		return fmt.Errorf("joker: %v", err)
 	}
+
+	relative := getRelative(fqdn, zone)
+
 	if d.config.Debug {
 		log.Infof("[%s] joker: removing entry %q from zone %q", domain, relative, zone)
 	}
 
-	response, err := d.jokerLogin()
+	response, err := d.login()
 	if err != nil {
 		return formatResponseError(response, err)
 	}
+
 	defer func() {
 		// Try to logout in case of errors
-		_, _ = d.jokerLogout()
+		_, _ = d.logout()
 	}()
 
-	response, err = d.jokerGetZone(zone)
+	response, err = d.getZone(zone)
 	if err != nil || response.StatusCode != 0 {
 		return formatResponseError(response, err)
 	}
 
 	dnsZone, modified := removeTxtEntryFromZone(response.Body, relative)
 	if modified {
-		response, err = d.jokerPutZone(zone, dnsZone)
+		response, err = d.putZone(zone, dnsZone)
 		if err != nil || response.StatusCode != 0 {
 			return formatResponseError(response, err)
 		}
 	}
 
-	response, err = d.jokerLogout()
+	response, err = d.logout()
 	if err != nil {
 		return formatResponseError(response, err)
 	}
 	return nil
 }
 
-// jokerLogin performs a login to Joker's DMAPI
-func (d *DNSProvider) jokerLogin() (*jokerResponse, error) {
-	if d.config.AuthSid != "" {
-		// already logged in
-		return nil, nil
-	}
-	response, err := d.postRequest("login", url.Values{"api-key": {d.config.APIKey}})
-	if err != nil {
-		return response, err
-	}
-	if response == nil {
-		return nil, fmt.Errorf("joker: login returned nil response")
-	}
-	if response.AuthSid == "" {
-		return response, fmt.Errorf("joker: login did not return valid Auth-Sid")
-	}
-	d.config.AuthSid = response.AuthSid
-	return response, nil
-}
-
-// jokerLogout closes authenticated session with Joker's DMAPI
-func (d *DNSProvider) jokerLogout() (*jokerResponse, error) {
-	if d.config.AuthSid != "" {
-		response, err := d.postRequest("logout", url.Values{})
-		if err == nil {
-			d.config.AuthSid = ""
-		}
-		return response, err
-	}
-	return nil, fmt.Errorf("joker: already logged out")
-}
-
-// jokerGetZone returns content of DNS zone for domain
-func (d *DNSProvider) jokerGetZone(domain string) (*jokerResponse, error) {
-	if d.config.AuthSid == "" {
-		return nil, fmt.Errorf("joker get zone: must be logged in")
-	}
-	response, err := d.postRequest("dns-zone-get", url.Values{"domain": {dns01.UnFqdn(domain)}})
-	return response, err
-}
-
-// jokerPutZone uploads DNS zone to Joker DMAPI
-func (d *DNSProvider) jokerPutZone(domain, zone string) (*jokerResponse, error) {
-	if d.config.AuthSid == "" {
-		return nil, fmt.Errorf("joker put zone: must be logged in")
-	}
-	response, err := d.postRequest("dns-zone-put", url.Values{"domain": {dns01.UnFqdn(domain)}, "zone": {strings.TrimSpace(zone)}})
-	if d.config.Debug {
-		log.Infof("[%s] dns-zone-put response headers:\n%v\n", domain, response.Headers)
-	}
-	return response, err
-}
-
-// postRequest performs actual HTTP request
-func (d *DNSProvider) postRequest(cmd string, data url.Values) (*jokerResponse, error) {
-	url := d.config.BaseURL + cmd
-	if d.config.AuthSid != "" {
-		data.Set("auth-sid", d.config.AuthSid)
-	}
-	if d.config.Debug {
-		log.Infof("postRequest:\n\tURL: %q\n\tData: %v", url, data)
-	}
-	resp, err := d.config.HTTPClient.PostForm(url, data)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	bodyString := strings.TrimSpace(string(body))
-	if resp.StatusCode >= 400 {
-		return nil, fmt.Errorf("HTTP error %d [%s]: %v", resp.StatusCode, http.StatusText(resp.StatusCode), body)
-	}
-
-	return parseJokerResponse(bodyString), nil
-}
-
-// Joker DMAPI Response
-type jokerResponse struct {
-	Headers    url.Values
-	Body       string
-	StatusCode int
-	StatusText string
-	AuthSid    string
-}
-
-// parseJokerResponse parses HTTP response body
-func parseJokerResponse(message string) *jokerResponse {
-	r := jokerResponse{Headers: url.Values{}, StatusCode: -1}
-	parts := strings.SplitN(message, "\n\n", 2)
-	if len(parts) > 0 {
-		for _, line := range strings.Split(parts[0], "\n") {
-			if strings.TrimSpace(line) != "" {
-				var key, val string
-				kv := strings.SplitN(line, ":", 2)
-				key = strings.TrimSuffix(kv[0], ":")
-				if len(kv) == 2 {
-					val = strings.TrimSpace(kv[1])
-				} else {
-					val = ""
-				}
-				r.Headers.Add(key, val)
-				switch key {
-				case "Status-Code":
-					i, err := strconv.Atoi(val)
-					if err == nil {
-						r.StatusCode = i
-					}
-				case "Status-Text":
-					r.StatusText = val
-				case "Auth-Sid":
-					r.AuthSid = val
-				}
-			}
-		}
-	}
-	if len(parts) > 1 {
-		r.Body = parts[1]
-	}
-	return &r
-}
-
-// getRecordNames returns base DNS domain, relative DNS name and value
-func getRecordNames(domain, keyAuth string) (zone, relative, value string, err error) {
-	fqdn, value := dns01.GetRecord(domain, keyAuth)
-	zone, err = dns01.FindZoneByFqdn(fqdn)
-	if err != nil {
-		return
-	}
-	relative = dns01.UnFqdn(strings.TrimSuffix(fqdn, dns01.ToFqdn(zone)))
-	return
+func getRelative(fqdn, zone string) string {
+	return dns01.UnFqdn(strings.TrimSuffix(fqdn, dns01.ToFqdn(zone)))
 }
 
 // formatResponseError formats error with optional details from DMAPI response
-func formatResponseError(response *jokerResponse, err error) error {
+func formatResponseError(response *response, err error) error {
 	if response != nil {
 		return fmt.Errorf("joker: DMAPI error: %v Response: %v", err, response.Headers)
 	}
 	return fmt.Errorf("joker: DMAPI error: %v", err)
-}
-
-// removeTxtEntryFromZone clean-ups all TXT records with given name
-func removeTxtEntryFromZone(zone, relative string) (string, bool) {
-	modified := false
-	var zoneEntries []string
-	prefix := fmt.Sprintf("%s TXT 0 ", relative)
-	for _, line := range strings.Split(zone, "\n") {
-		if strings.HasPrefix(line, prefix) {
-			modified = true
-			continue
-		}
-		zoneEntries = append(zoneEntries, line)
-	}
-	return strings.TrimSpace(strings.Join(zoneEntries, "\n")), modified
-}
-
-// addTxtEntryToZone returns DNS zone with added TXT record
-func addTxtEntryToZone(zone, relative, value string, ttl int) string {
-	// prefixForOldEntries := fmt.Sprintf("%s TXT 0 ", relative)
-	var zoneEntries []string
-	for _, line := range strings.Split(zone, "\n") {
-		// if strings.HasPrefix(line, prefixForOldEntries) {
-		// 	continue
-		// }
-		zoneEntries = append(zoneEntries, fixTxtLines(line))
-	}
-
-	newZoneEntry := fmt.Sprintf("%s TXT 0 %q %d", relative, value, ttl)
-	zoneEntries = append(zoneEntries, newZoneEntry)
-
-	return strings.TrimSpace(strings.Join(zoneEntries, "\n"))
-}
-
-// Temporary workaround, until it get fixed on API side
-func fixTxtLines(line string) (output string) {
-	output = line
-	fields := strings.Fields(line)
-	if len(fields) < 6 || fields[1] != "TXT" {
-		return
-	}
-	if fields[3][0] == '"' && fields[4] == `"` {
-		fields[3] = strings.TrimSpace(fields[3]) + `"`
-		fields = append(fields[:4], fields[5:]...)
-	}
-	return strings.Join(fields, " ")
 }

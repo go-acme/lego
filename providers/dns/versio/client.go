c0db99716141406479324125fcbeea86b3f08b14
Frans Kuipers <franskuipers@gmail.com>
2019-06-21 10:17:39 +0200

    Add dns-01 provider versio.nl

---
+++ providers/dns/versio/client.go
@@ -0,0 +1,134 @@
+package versio
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path"
+)
+
+const defaultBaseURL = "https://www.versio.nl/api/v1/"
+
+type dnsRecordsResponse struct {
+	Record dnsRecord `json:"domainInfo"`
+}
+
+type dnsRecord struct {
+	DNSRecords []record `json:"dns_records"`
+}
+
+type record struct {
+	Type  string `json:"type,omitempty"`
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
+	Prio  int    `json:"prio,omitempty"`
+	TTL   int    `json:"ttl,omitempty"`
+}
+
+type dnsErrorResponse struct {
+	Error errorMessage `json:"error"`
+}
+
+type errorMessage struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+func (d *DNSProvider) postDNSRecords(domain string, msg interface{}) error {
+
+	reqBody := &bytes.Buffer{}
+	err := json.NewEncoder(reqBody).Encode(msg)
+	if err != nil {
+		return err
+	}
+
+	newURI := path.Join(d.config.BaseURL.EscapedPath(), "domains/"+domain+"/update")
+	endpoint, err := d.config.BaseURL.Parse(newURI)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, endpoint.String(), reqBody)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	if len(d.config.Username) > 0 && len(d.config.Password) > 0 {
+		req.SetBasicAuth(d.config.Username, d.config.Password)
+	}
+
+	resp, err := d.config.HTTPClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= http.StatusBadRequest {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("%d: failed to read response body: %v", resp.StatusCode, err)
+		}
+
+		respError := &dnsErrorResponse{}
+		err = json.Unmarshal(body, respError)
+		return fmt.Errorf("%d: request failed: %v", resp.StatusCode, respError.Error.Message)
+	}
+
+	return nil
+}
+
+func (d *DNSProvider) getDNSRecords(domain string) (*dnsRecordsResponse, error) {
+	reqBody := &bytes.Buffer{}
+
+	newURI := path.Join(d.config.BaseURL.EscapedPath(), "domains/"+domain+"?show_dns_records=true")
+	endpoint, err := d.config.BaseURL.Parse(newURI)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodGet, endpoint.String(), reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	if len(d.config.Username) > 0 && len(d.config.Password) > 0 {
+		req.SetBasicAuth(d.config.Username, d.config.Password)
+	}
+
+	resp, err := d.config.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= http.StatusBadRequest {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("%d: failed to read response body: %v", resp.StatusCode, err)
+		}
+
+		respError := &dnsErrorResponse{}
+		err = json.Unmarshal(body, respError)
+		return nil, fmt.Errorf("%d: request failed: %v", resp.StatusCode, respError.Error.Message)
+	}
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("request failed: Body: %s Err: %v", string(content), err)
+	}
+	// Everything looks good; we'll need all the dns_records to add the new TXT record
+	respData := &dnsRecordsResponse{}
+	err = json.Unmarshal(content, respData)
+	if err != nil {
+		return nil, fmt.Errorf("%v: %s", err, content)
+	}
+
+	return respData, nil
+
+}
---
+++ providers/dns/versio/versio_test.go
@@ -0,0 +1,345 @@
+// Package vegadns implements a DNS provider for solving the DNS-01
+// challenge using VegaDNS.
+package versio
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/go-acme/lego/platform/tester"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const testDomain = "example.com"
+
+const ipPort = "127.0.0.1:2112"
+
+var envVars = map[string]string{
+	"VERSIO_USERNAME": "me@example.com",
+	"VERSIO_PASSWORD": "secret",
+	"VERSIO_ENDPOINT": "http://127.0.0.1:2112",
+}
+
+var envTest = tester.NewEnvTest("VERSIO_USERNAME", "VERSIO_PASSWORD", "VERSIO_ENDPOINT")
+
+type muxCallback func() *http.ServeMux
+
+func TestNewDNSProvider(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		envVars  map[string]string
+		expected string
+	}{
+		{
+			desc: "success",
+			envVars: map[string]string{
+				"VERSIO_USERNAME": "me@example.com",
+				"VERSIO_PASSWORD": "SECRET",
+			},
+		},
+		{
+			desc: "missing token",
+			envVars: map[string]string{
+				"VERSIO_PASSWORD": "me@example.com",
+			},
+			expected: "versio: some credentials information are missing: VERSIO_USERNAME",
+		},
+		{
+			desc: "missing key",
+			envVars: map[string]string{
+				"VERSIO_USERNAME": "TOKEN",
+			},
+			expected: "versio: some credentials information are missing: VERSIO_PASSWORD",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			defer envTest.RestoreEnv()
+			envTest.ClearEnv()
+
+			envTest.Apply(test.envVars)
+
+			p, err := NewDNSProvider()
+
+			if len(test.expected) == 0 {
+				require.NoError(t, err)
+				require.NotNil(t, p)
+				require.NotNil(t, p.config)
+			} else {
+				require.EqualError(t, err, test.expected)
+			}
+		})
+	}
+}
+
+func TestNewDNSProviderConfig(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		config   *Config
+		expected string
+	}{
+		{
+			desc: "success",
+			config: &Config{
+				Username: "me@example.com",
+				Password: "PW",
+			},
+		},
+		{
+			desc:     "nil config",
+			config:   nil,
+			expected: "versio: the configuration of the DNS provider is nil",
+		},
+		{
+			desc: "missing username",
+			config: &Config{
+				Password: "PW",
+			},
+			expected: "versio: the versio username is missing",
+		},
+		{
+			desc: "missing password",
+			config: &Config{
+				Username: "UN",
+			},
+			expected: "versio: the versio password is missing",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			p, err := NewDNSProviderConfig(test.config)
+
+			if len(test.expected) == 0 {
+				require.NoError(t, err)
+				require.NotNil(t, p)
+				require.NotNil(t, p.config)
+			} else {
+				require.EqualError(t, err, test.expected)
+			}
+		})
+	}
+}
+
+func TestDNSProvider_TimeoutSuccess(t *testing.T) {
+	defer envTest.RestoreEnv()
+	envTest.ClearEnv()
+
+	ts, err := startTestServer(muxSuccess)
+	require.NoError(t, err)
+
+	defer ts.Close()
+
+	envTest.Apply(envVars)
+	provider, err := NewDNSProvider()
+	require.NoError(t, err)
+
+	timeout, interval := provider.Timeout()
+	assert.Equal(t, timeout, time.Duration(60000000000))
+	assert.Equal(t, interval, time.Duration(5000000000))
+}
+
+func TestDNSProvider_SequentialSuccess(t *testing.T) {
+	defer envTest.RestoreEnv()
+	envTest.ClearEnv()
+
+	ts, err := startTestServer(muxSuccess)
+	require.NoError(t, err)
+
+	defer ts.Close()
+
+	envTest.Apply(envVars)
+	provider, err := NewDNSProvider()
+	require.NoError(t, err)
+
+	sequential := provider.Sequential()
+	assert.Equal(t, sequential, time.Duration(60000000000))
+}
+
+func TestDNSProvider_Present(t *testing.T) {
+	testCases := []struct {
+		desc          string
+		callback      muxCallback
+		expectedError string
+	}{
+		{
+			desc:     "Success",
+			callback: muxSuccess,
+		},
+		{
+			desc:          "FailToFindZone",
+			callback:      muxFailToFindZone,
+			expectedError: "versio: 401: request failed: ObjectDoesNotExist|Domain not found",
+		},
+		{
+			desc:          "FailToCreateTXT",
+			callback:      muxFailToCreateTXT,
+			expectedError: "versio: 400: request failed: ProcessError|DNS record invalid type _acme-challenge.fjmk.eu. TST",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			defer envTest.RestoreEnv()
+			envTest.ClearEnv()
+
+			ts, err := startTestServer(test.callback)
+			require.NoError(t, err)
+
+			defer ts.Close()
+
+			envTest.Apply(envVars)
+			provider, err := NewDNSProvider()
+			require.NoError(t, err)
+
+			err = provider.Present(testDomain, "token", "keyAuth")
+			if len(test.expectedError) == 0 {
+				assert.NoError(t, err)
+			} else {
+				assert.EqualError(t, err, test.expectedError)
+			}
+		})
+	}
+}
+
+func TestDNSProvider_CleanUp(t *testing.T) {
+	testCases := []struct {
+		desc          string
+		callback      muxCallback
+		expectedError string
+	}{
+		{
+			desc:     "Success",
+			callback: muxSuccess,
+		},
+		{
+			desc:          "FailToFindZone",
+			callback:      muxFailToFindZone,
+			expectedError: "versio: 401: request failed: ObjectDoesNotExist|Domain not found",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			defer envTest.RestoreEnv()
+			envTest.ClearEnv()
+
+			ts, err := startTestServer(test.callback)
+			require.NoError(t, err)
+
+			defer ts.Close()
+
+			envTest.Apply(envVars)
+			provider, err := NewDNSProvider()
+			require.NoError(t, err)
+
+			err = provider.CleanUp(testDomain, "token", "keyAuth")
+			if len(test.expectedError) == 0 {
+				assert.NoError(t, err)
+			} else {
+				assert.EqualError(t, err, test.expectedError)
+			}
+		})
+	}
+}
+
+func muxSuccess() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/domains/example.com", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet && r.URL.Query().Get("show_dns_records") == "true" {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, tokenResponseMock)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+	})
+
+	mux.HandleFunc("/domains/example.com/update", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, tokenResponseMock)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+	})
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Printf("Not Found for Request: (%+v)\n\n", r)
+	})
+
+	return mux
+}
+
+func muxFailToFindZone() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/domains/example.com", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, tokenFailToFindZoneMock)
+	})
+
+	return mux
+}
+
+func muxFailToCreateTXT() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/domains/example.com", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet && r.URL.Query().Get("show_dns_records") == "true" {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, tokenResponseMock)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+	})
+
+	mux.HandleFunc("/domains/example.com/update", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, tokenFailToCreateTXTMock)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+	})
+
+	return mux
+}
+
+// Starts and returns a test server using a custom ip/port. Defer close() afterwards.
+func startTestServer(callback muxCallback) (*httptest.Server, error) {
+	err := os.Setenv("SECRET_VEGADNS_KEY", "key")
+	if err != nil {
+		return nil, err
+	}
+
+	err = os.Setenv("SECRET_VEGADNS_SECRET", "secret")
+	if err != nil {
+		return nil, err
+	}
+
+	err = os.Setenv("VEGADNS_URL", "http://"+ipPort)
+	if err != nil {
+		return nil, err
+	}
+
+	ts := httptest.NewUnstartedServer(callback())
+
+	l, err := net.Listen("tcp", ipPort)
+	if err != nil {
+		return nil, err
+	}
+
+	ts.Listener = l
+	ts.Start()
+
+	return ts, nil
+}

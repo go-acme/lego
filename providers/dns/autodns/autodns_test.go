570fc61be9406e0294d7e638228a696d80f07f32
klg <k.langenberg@imi.de>
2019-08-30 15:25:32 +0200

    Added more tests

--- providers/dns/autodns/autodns_test.go
+++ providers/dns/autodns/autodns_test.go
@@ -1,6 +1,8 @@
 package autodns
 
 import (
+	"net/http"
+	"net/url"
 	"reflect"
 	"testing"
 
@@ -8,18 +10,58 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var envTest = tester.NewEnvTest(envApiEndpoint)
+var envTest = tester.NewEnvTest(envApiEndpoint, envApiUser, envApiPassword)
 
 func TestNewDNSProvider(t *testing.T) {
+	defaultEndpointURL, _ := url.Parse(defaultEndpoint)
+	examplEndpointUrl, _ := url.Parse(demoEndpoint)
+
 	tests := []struct {
 		name    string
 		want    *DNSProvider
 		wantErr bool
+		env     map[string]string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "complete, no errors",
+			want: &DNSProvider{
+				config: &Config{
+					Endpoint:   defaultEndpointURL,
+					Username:   "test",
+					Password:   "1234",
+					Context:    4,
+					HTTPClient: &http.Client{},
+				},
+			},
+			env: map[string]string{
+				envApiUser:     "test",
+				envApiPassword: "1234",
+			},
+		},
+		{
+			name: "different endpoint url",
+			want: &DNSProvider{
+				config: &Config{
+					Endpoint:   examplEndpointUrl,
+					Username:   "test",
+					Password:   "1234",
+					Context:    4,
+					HTTPClient: &http.Client{},
+				},
+			},
+			env: map[string]string{
+				envApiUser:     "test",
+				envApiPassword: "1234",
+				envApiEndpoint: demoEndpoint,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			defer envTest.RestoreEnv()
+			envTest.ClearEnv()
+			envTest.Apply(tt.env)
+
 			got, err := NewDNSProvider()
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NewDNSProvider() error = %v, wantErr %v", err, tt.wantErr)

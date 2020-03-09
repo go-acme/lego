ce2cc584852cebce6b1e95758879d01342b96329
Ludovic Fernandez <ldez@users.noreply.github.com>
2020-03-09 09:43:27 +0100

    chore: generate DNS table in the README. (#1080)

--- internal/dnsdocs/generator.go
+++ internal/dnsdocs/generator.go
@@ -3,11 +3,17 @@ package main
 //go:generate go run .
 
 import (
+	"bufio"
 	"bytes"
+	"errors"
+	"fmt"
 	"go/format"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 	"text/template"
 
@@ -21,6 +27,12 @@ const (
 	cliTemplate = root + "internal/dnsdocs/dns.go.tmpl"
 	cliOutput   = root + "cmd/zz_gen_cmd_dnshelp.go"
 	docOutput   = root + "docs/content/dns"
+	readmePath  = root + "README.md"
+)
+
+const (
+	startLine = "<!-- START DNS PROVIDERS LIST -->"
+	endLine   = "<!-- END DNS PROVIDERS LIST -->"
 )
 
 type Model struct {
@@ -63,6 +75,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// generate README.md
+	err = generateReadMe(models)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Documentation for %d DNS providers has been generated.\n", len(models.Providers)+1)
 }
 
 func walker(prs *Providers) func(string, os.FileInfo, error) error {
@@ -132,5 +152,135 @@ func generateCLIHelp(models *Providers) error {
 	}
 
 	_, err = file.Write(source)
+	return err
+}
+
+func generateReadMe(models *Providers) error {
+	max, lines := extractTableData(models)
+
+	file, err := os.Open(readmePath)
+	if err != nil {
+		return err
+	}
+
+	defer func() { _ = file.Close() }()
+
+	var skip bool
+
+	buffer := bytes.NewBufferString("")
+
+	fileScanner := bufio.NewScanner(file)
+	for fileScanner.Scan() {
+		text := fileScanner.Text()
+
+		if text == startLine {
+			_, _ = fmt.Fprintln(buffer, text)
+			err = writeDNSTable(buffer, lines, max)
+			if err != nil {
+				return err
+			}
+			skip = true
+		}
+
+		if text == endLine {
+			skip = false
+		}
+
+		if skip {
+			continue
+		}
+
+		_, _ = fmt.Fprintln(buffer, text)
+	}
+
+	if fileScanner.Err() != nil {
+		return fileScanner.Err()
+	}
+
+	if skip {
+		return errors.New("missing end tag")
+	}
+
+	return ioutil.WriteFile(readmePath, buffer.Bytes(), 0666)
+}
+
+func extractTableData(models *Providers) (int, [][]string) {
+	readmePattern := "[%s](https://go-acme.github.io/lego/dns/%s/)"
+
+	items := []string{fmt.Sprintf(readmePattern, "Manual", "manual")}
+
+	var max int
+
+	for _, pvd := range models.Providers {
+		item := fmt.Sprintf(readmePattern, strings.ReplaceAll(pvd.Name, "|", "/"), pvd.Code)
+		items = append(items, item)
+
+		if max < len(item) {
+			max = len(item)
+		}
+	}
+
+	const nbCol = 4
+
+	sort.Slice(items, func(i, j int) bool {
+		return strings.ToLower(items[i]) < strings.ToLower(items[j])
+	})
+
+	var lines [][]string
+	var line []string
+
+	for i, item := range items {
+		switch {
+		case len(line) == nbCol:
+			lines = append(lines, line)
+			line = []string{item}
+
+		case i == len(models.Providers)-1:
+			line = append(line, item)
+			for j := len(line); j < nbCol; j++ {
+				line = append(line, "")
+			}
+			lines = append(lines, line)
+
+		default:
+			line = append(line, item)
+		}
+	}
+
+	if len(line) < nbCol {
+		for j := len(line); j < nbCol; j++ {
+			line = append(line, "")
+		}
+		lines = append(lines, line)
+	}
+
+	return max, lines
+}
+
+func writeDNSTable(w io.Writer, lines [][]string, size int) error {
+	_, err := fmt.Fprintf(w, "\n")
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(w, "|%[1]s|%[1]s|%[1]s|%[1]s|\n", strings.Repeat(" ", size+2))
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(w, "|%[1]s|%[1]s|%[1]s|%[1]s|\n", strings.Repeat("-", size+2))
+	if err != nil {
+		return err
+	}
+
+	linePattern := fmt.Sprintf("| %%-%[1]ds | %%-%[1]ds | %%-%[1]ds | %%-%[1]ds |\n", size)
+	for _, line := range lines {
+		_, err = fmt.Fprintf(w, linePattern, line[0], line[1], line[2], line[3])
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = fmt.Fprintf(w, "\n")
 	return err
 }

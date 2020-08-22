f17ae2ec025c0eec0895a3069e4c3be97043a38e
kkm <kubasurdej@gmail.com>
2020-08-22 18:25:32 +0200

    Test all client GETs

--- providers/dns/hyperone/internal/client_test.go
+++ providers/dns/hyperone/internal/client_test.go
@@ -19,6 +19,59 @@ func (s signerMock) GetJWT() (string, error) {
 	return "", nil
 }
 
+func TestClient_FindRecordset(t *testing.T) {
+	server := createTestServer(t, "GET", "/dns/loc123/project/proj123/zone/321/recordset", "recordset.json")
+	client := getTestClient(t, server.URL)
+
+	recordset, err := client.FindRecordset("321", "SOA", "example.com.")
+	require.NoError(t, err)
+
+	expected := &Recordset{
+		ID:         "123456789abcd",
+		Name:       "example.com.",
+		RecordType: "SOA",
+		TTL:        1800,
+	}
+
+	assert.Equal(t, expected, recordset)
+}
+
+func TestClient_GetRecords(t *testing.T) {
+	server := createTestServer(t, "GET", "/dns/loc123/project/proj123/zone/321/recordset/322/record", "record.json")
+	client := getTestClient(t, server.URL)
+
+	records, err := client.GetRecords("321", "322")
+	require.NoError(t, err)
+
+	expected := []Record{
+		{
+			ID:      "135128352183572dd",
+			Content: "pns.hyperone.com. hostmaster.hyperone.com. 1 15 180 1209600 1800",
+			Enabled: true,
+		},
+	}
+
+	assert.Equal(t, expected, records)
+}
+
+func TestClient_FindZone(t *testing.T) {
+	server := createTestServer(t, "GET", "/dns/loc123/project/proj123/zone", "zones.json")
+	client := getTestClient(t, server.URL)
+
+	zone, err := client.FindZone("example.com")
+	require.NoError(t, err)
+
+	expected := &Zone{
+		ID:      "zoneB",
+		Name:    "example.com",
+		DNSName: "example.com",
+		FQDN:    "example.com.",
+		URI:     "",
+	}
+
+	assert.Equal(t, expected, zone)
+}
+
 func TestClient_GetZones(t *testing.T) {
 	server := createTestServer(t, "GET", "/dns/loc123/project/proj123/zone", "zones.json")
 	client := getTestClient(t, server.URL)

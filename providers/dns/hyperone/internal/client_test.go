8411239af40dbb0763e903225cdade33ad0c1b3e
kkm <kubasurdej@gmail.com>
2020-08-22 17:22:39 +0200

    Move server creation in test to helper method

--- providers/dns/hyperone/internal/client_test.go
+++ providers/dns/hyperone/internal/client_test.go
@@ -20,28 +20,8 @@ func (s signerMock) GetJWT() (string, error) {
 }
 
 func TestClient_GetZones(t *testing.T) {
-	mux := http.NewServeMux()
-	server := httptest.NewServer(mux)
+	server := createTestServer("GET", "/dns/loc123/project/proj123/zone", "zones.json")
 	t.Cleanup(server.Close)
-
-	mux.Handle("/dns/loc123/project/proj123/zone", http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		if req.Method != http.MethodGet {
-			http.Error(rw, fmt.Sprintf("unsupported method: %s", req.Method), http.StatusMethodNotAllowed)
-			return
-		}
-
-		file, err := os.Open(filepath.Join(".", "fixtures", "zones.json"))
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		_, err = io.Copy(rw, file)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	}))
 
 	passport := &Passport{
 		SubjectID: "/iam/project/proj123/sa/xxxxxxx",
@@ -73,4 +53,30 @@ func TestClient_GetZones(t *testing.T) {
 	}
 
 	assert.Equal(t, expected, zones)
+}
+
+func createTestServer(method, path, fixtureName string) *httptest.Server {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+
+	mux.Handle(path, http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if req.Method != method {
+			http.Error(rw, fmt.Sprintf("unsupported method: %s", req.Method), http.StatusMethodNotAllowed)
+			return
+		}
+
+		file, err := os.Open(filepath.Join(".", "fixtures", fixtureName))
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		_, err = io.Copy(rw, file)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}))
+
+	return server
 }

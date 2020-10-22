918037b8aff5f0fd6db9d5c96ef49608dfe403b7
Ludovic Fernandez <ldez@users.noreply.github.com>
2020-10-22 22:30:50 +0200

    chore: update linter. (#1275)

--- cmd/certs_storage.go
+++ cmd/certs_storage.go
@@ -197,7 +197,7 @@ func (s *CertificatesStorage) MoveToArchive(domain string) error {
 
 // sanitizedDomain Make sure no funny chars are in the cert names (like wildcards ;)).
 func sanitizedDomain(domain string) string {
-	safe, err := idna.ToASCII(strings.Replace(domain, "*", "_", -1))
+	safe, err := idna.ToASCII(strings.ReplaceAll(domain, "*", "_"))
 	if err != nil {
 		log.Fatal(err)
 	}

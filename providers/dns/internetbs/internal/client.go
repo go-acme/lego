4f7010cb2eb7af78d9339dec0efe424e90017ea9
Fernandez Ludovic <ldez@users.noreply.github.com>
2021-06-11 21:44:49 +0200

    feat: add internet.bs

---
+++ providers/dns/internetbs/internal/client.go
@@ -0,0 +1,140 @@
+package internal
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
+	"unicode"
+
+	querystring "github.com/google/go-querystring/query"
+)
+
+const baseURL = "https://api.internet.bs"
+
+// status SUCCESS, PENDING, FAILURE.
+const statusSuccess = "SUCCESS"
+
+// Client is the API client.
+type Client struct {
+	HTTPClient *http.Client
+	baseURL    *url.URL
+	debug      bool
+
+	apiKey   string
+	password string
+}
+
+// NewClient creates a new Client.
+func NewClient(apiKey string, password string) *Client {
+	baseURL, _ := url.Parse(baseURL)
+
+	return &Client{
+		HTTPClient: &http.Client{Timeout: 10 * time.Second},
+		baseURL:    baseURL,
+		apiKey:     apiKey,
+		password:   password,
+	}
+}
+
+// AddRecord The command is intended to add a new DNS record to a specific zone (domain).
+func (c Client) AddRecord(query RecordQuery) error {
+	var r APIResponse
+	err := c.do("Add", query, &r)
+	if err != nil {
+		return err
+	}
+
+	if r.Status != statusSuccess {
+		return r
+	}
+
+	return nil
+}
+
+// RemoveRecord The command is intended to remove a DNS record from a specific zone.
+func (c Client) RemoveRecord(query RecordQuery) error {
+	var r APIResponse
+	err := c.do("Remove", query, &r)
+	if err != nil {
+		return err
+	}
+
+	if r.Status != statusSuccess {
+		return r
+	}
+
+	return nil
+}
+
+// ListRecords The command is intended to retrieve the list of DNS records for a specific domain.
+func (c Client) ListRecords(query ListRecordQuery) ([]Record, error) {
+	var l ListResponse
+	err := c.do("List", query, &l)
+	if err != nil {
+		return nil, err
+	}
+
+	if l.Status != statusSuccess {
+		return nil, l.APIResponse
+	}
+
+	return l.Records, nil
+}
+
+func (c Client) do(action string, params interface{}, response interface{}) error {
+	endpoint, err := c.baseURL.Parse(path.Join(c.baseURL.Path, "Domain", "DnsRecord", action))
+	if err != nil {
+		return fmt.Errorf("create endpoint: %w", err)
+	}
+
+	values, err := querystring.Values(params)
+	if err != nil {
+		return fmt.Errorf("parse query parameters: %w", err)
+	}
+
+	values.Set("apiKey", c.apiKey)
+	values.Set("password", c.password)
+	values.Set("ResponseFormat", "JSON")
+
+	resp, err := c.HTTPClient.PostForm(endpoint.String(), values)
+	if err != nil {
+		return fmt.Errorf("post request: %w", err)
+	}
+
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode/100 != 2 {
+		data, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("status code: %d, %s", resp.StatusCode, string(data))
+	}
+
+	if c.debug {
+		return dump(endpoint, resp, response)
+	}
+
+	return json.NewDecoder(resp.Body).Decode(response)
+}
+
+func dump(endpoint *url.URL, resp *http.Response, response interface{}) error {
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	fields := strings.FieldsFunc(endpoint.Path, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	})
+
+	err = ioutil.WriteFile(filepath.Join("fixtures", strings.Join(fields, "_")+".json"), data, 0o666)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(data, response)
+}
---
+++ providers/dns/internetbs/internetbs.go
@@ -0,0 +1,135 @@
+// Package internetbs implements a DNS provider for solving the DNS-01 challenge using internet.bs.
+package internetbs
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-acme/lego/v4/challenge/dns01"
+	"github.com/go-acme/lego/v4/platform/config/env"
+	"github.com/go-acme/lego/v4/providers/dns/internetbs/internal"
+)
+
+// Environment variables names.
+const (
+	envNamespace = "INTERNET_BS_"
+
+	EnvAPIKey   = envNamespace + "API_KEY"
+	EnvPassword = envNamespace + "PASSWORD"
+
+	EnvTTL                = envNamespace + "TTL"
+	EnvPropagationTimeout = envNamespace + "PROPAGATION_TIMEOUT"
+	EnvPollingInterval    = envNamespace + "POLLING_INTERVAL"
+	EnvHTTPTimeout        = envNamespace + "HTTP_TIMEOUT"
+)
+
+// Config is used to configure the creation of the DNSProvider.
+type Config struct {
+	APIKey             string
+	Password           string
+	PropagationTimeout time.Duration
+	PollingInterval    time.Duration
+	TTL                int
+	HTTPClient         *http.Client
+}
+
+// NewDefaultConfig returns a default configuration for the DNSProvider.
+func NewDefaultConfig() *Config {
+	return &Config{
+		TTL:                env.GetOrDefaultInt(EnvTTL, 3600),
+		PropagationTimeout: env.GetOrDefaultSecond(EnvPropagationTimeout, dns01.DefaultPropagationTimeout),
+		PollingInterval:    env.GetOrDefaultSecond(EnvPollingInterval, dns01.DefaultPollingInterval),
+		HTTPClient: &http.Client{
+			Timeout: env.GetOrDefaultSecond(EnvHTTPTimeout, 30*time.Second),
+		},
+	}
+}
+
+// DNSProvider implements the challenge.Provider interface.
+type DNSProvider struct {
+	config *Config
+	client *internal.Client
+}
+
+// NewDNSProvider returns a DNSProvider instance configured for internet.bs.
+// Credentials must be passed in the environment variable: INTERNET_BS_API_KEY, INTERNET_BS_PASSWORD.
+func NewDNSProvider() (*DNSProvider, error) {
+	values, err := env.Get(EnvAPIKey, EnvPassword)
+	if err != nil {
+		return nil, fmt.Errorf("internetbs: %w", err)
+	}
+
+	config := NewDefaultConfig()
+	config.APIKey = values[EnvAPIKey]
+	config.Password = values[EnvPassword]
+
+	return NewDNSProviderConfig(config)
+}
+
+// NewDNSProviderConfig return a DNSProvider instance configured for internet.bs.
+func NewDNSProviderConfig(config *Config) (*DNSProvider, error) {
+	if config == nil {
+		return nil, errors.New("internetbs: the configuration of the DNS provider is nil")
+	}
+
+	if config.APIKey == "" || config.Password == "" {
+		return nil, errors.New("internetbs: missing credentials")
+	}
+
+	client := internal.NewClient(config.APIKey, config.Password)
+
+	if config.HTTPClient != nil {
+		client.HTTPClient = config.HTTPClient
+	}
+
+	return &DNSProvider{
+		config: config,
+		client: client,
+	}, nil
+}
+
+// Timeout returns the timeout and interval to use when checking for DNS propagation.
+// Adjusting here to cope with spikes in propagation times.
+func (d *DNSProvider) Timeout() (timeout, interval time.Duration) {
+	return d.config.PropagationTimeout, d.config.PollingInterval
+}
+
+// Present creates a TXT record using the specified parameters.
+func (d *DNSProvider) Present(domain, token, keyAuth string) error {
+	fqdn, value := dns01.GetRecord(domain, keyAuth)
+
+	query := internal.RecordQuery{
+		FullRecordName: dns01.UnFqdn(fqdn),
+		Type:           "TXT",
+		Value:          value,
+		TTL:            d.config.TTL,
+	}
+
+	err := d.client.AddRecord(query)
+	if err != nil {
+		return fmt.Errorf("internetbs: %w", err)
+	}
+
+	return nil
+}
+
+// CleanUp removes the TXT record matching the specified parameters.
+func (d *DNSProvider) CleanUp(domain, token, keyAuth string) error {
+	fqdn, value := dns01.GetRecord(domain, keyAuth)
+
+	query := internal.RecordQuery{
+		FullRecordName: dns01.UnFqdn(fqdn),
+		Type:           "TXT",
+		Value:          value,
+		TTL:            d.config.TTL,
+	}
+
+	err := d.client.RemoveRecord(query)
+	if err != nil {
+		return fmt.Errorf("internetbs: %w", err)
+	}
+
+	return nil
+}

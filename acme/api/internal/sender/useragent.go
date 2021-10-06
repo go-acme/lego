896cf568d6022dfc77ba473996c088446221d332
Fernandez Ludovic <ldez@users.noreply.github.com>
2021-10-06 12:04:16 +0200

    Prepare release v4.5.3

--- acme/api/internal/sender/useragent.go
+++ acme/api/internal/sender/useragent.go
@@ -5,10 +5,10 @@ package sender
 
 const (
 	// ourUserAgent is the User-Agent of this underlying library package.
-	ourUserAgent = "xenolf-acme/4.4.3"
+	ourUserAgent = "xenolf-acme/4.5.3"
 
 	// ourUserAgentComment is part of the UA comment linked to the version status of this underlying library package.
 	// values: detach|release
 	// NOTE: Update this with each tagged release.
-	ourUserAgentComment = "detach"
+	ourUserAgentComment = "release"
 )

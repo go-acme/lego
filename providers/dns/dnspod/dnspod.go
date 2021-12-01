f4306475b92b67485a7ddb530b5c9a220b8283c8
libla <libla@qq.com>
2021-12-01 10:48:13 +0800

    upgrade dnspod api

--- providers/dns/dnspod/dnspod.go
+++ providers/dns/dnspod/dnspod.go
@@ -4,21 +4,25 @@ package dnspod
 import (
 	"errors"
 	"fmt"
-	"net/http"
-	"strconv"
+	"golang.org/x/net/idna"
+	"math"
 	"strings"
 	"time"
 
 	"github.com/go-acme/lego/v4/challenge/dns01"
 	"github.com/go-acme/lego/v4/platform/config/env"
-	"github.com/nrdcg/dnspod-go"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
+	dnspod "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/dnspod/v20210323"
 )
 
 // Environment variables names.
 const (
 	envNamespace = "DNSPOD_"
 
-	EnvAPIKey = envNamespace + "API_KEY"
+	EnvSecretID     = envNamespace + "SECRET_ID"
+	EnvSecretKey    = envNamespace + "SECRET_KEY"
+	EnvSessionToken = envNamespace + "SESSION_TOKEN"
 
 	EnvTTL                = envNamespace + "TTL"
 	EnvPropagationTimeout = envNamespace + "PROPAGATION_TIMEOUT"
@@ -28,11 +32,13 @@ const (
 
 // Config is used to configure the creation of the DNSProvider.
 type Config struct {
-	LoginToken         string
+	SecretID           string
+	SecretKey          string
+	SessionToken       string
 	TTL                int
 	PropagationTimeout time.Duration
 	PollingInterval    time.Duration
-	HTTPClient         *http.Client
+	HTTPTimeout        time.Duration
 }
 
 // NewDefaultConfig returns a default configuration for the DNSProvider.
@@ -41,9 +47,7 @@ func NewDefaultConfig() *Config {
 		TTL:                env.GetOrDefaultInt(EnvTTL, 600),
 		PropagationTimeout: env.GetOrDefaultSecond(EnvPropagationTimeout, dns01.DefaultPropagationTimeout),
 		PollingInterval:    env.GetOrDefaultSecond(EnvPollingInterval, dns01.DefaultPollingInterval),
-		HTTPClient: &http.Client{
-			Timeout: env.GetOrDefaultSecond(EnvHTTPTimeout, 30*time.Second),
-		},
+		HTTPTimeout:        env.GetOrDefaultSecond(EnvHTTPTimeout, 30*time.Second),
 	}
 }
 
@@ -56,13 +60,15 @@ type DNSProvider struct {
 // NewDNSProvider returns a DNSProvider instance configured for dnspod.
 // Credentials must be passed in the environment variables: DNSPOD_API_KEY.
 func NewDNSProvider() (*DNSProvider, error) {
-	values, err := env.Get(EnvAPIKey)
+	values, err := env.Get(EnvSecretID, EnvSecretKey)
 	if err != nil {
 		return nil, fmt.Errorf("dnspod: %w", err)
 	}
 
 	config := NewDefaultConfig()
-	config.LoginToken = values[EnvAPIKey]
+	config.SecretID = values[EnvSecretID]
+	config.SecretKey = values[EnvSecretKey]
+	config.SessionToken = env.GetOrFile(EnvSessionToken)
 
 	return NewDNSProviderConfig(config)
 }
@@ -73,14 +79,24 @@ func NewDNSProviderConfig(config *Config) (*DNSProvider, error) {
 		return nil, errors.New("dnspod: the configuration of the DNS provider is nil")
 	}
 
-	if config.LoginToken == "" {
-		return nil, errors.New("dnspod: credentials missing")
+	var credential *common.Credential
+
+	switch {
+	case config.SecretID != "" && config.SecretKey != "" && config.SessionToken != "":
+		credential = common.NewTokenCredential(config.SecretID, config.SecretKey, config.SessionToken)
+	case config.SecretID != "" && config.SecretKey != "":
+		credential = common.NewCredential(config.SecretID, config.SecretKey)
+	default:
+		return nil, fmt.Errorf("dnspod: credentials missing")
 	}
 
-	params := dnspod.CommonParams{LoginToken: config.LoginToken, Format: "json"}
-
-	client := dnspod.NewClient(params)
-	client.HTTPClient = config.HTTPClient
+	cpf := profile.NewClientProfile()
+	cpf.HttpProfile.Endpoint = "dnspod.tencentcloudapi.com"
+	cpf.HttpProfile.ReqTimeout = int(math.Round(config.HTTPTimeout.Seconds()))
+	client, err := dnspod.NewClient(credential, "", cpf)
+	if err != nil {
+		return nil, fmt.Errorf("dnspod: credentials failed: %w", err)
+	}
 
 	return &DNSProvider{client: client, config: config}, nil
 }
@@ -92,11 +108,22 @@ func (d *DNSProvider) Present(domain, token, keyAuth string) error {
 	if err != nil {
 		return err
 	}
-
-	recordAttributes := d.newTxtRecord(zoneName, fqdn, value, d.config.TTL)
-	_, _, err = d.client.Records.Create(zoneID, *recordAttributes)
+	recordName, err := extractRecordName(fqdn, zoneName)
 	if err != nil {
-		return fmt.Errorf("API call failed: %w", err)
+		return err
+	}
+	request := dnspod.NewCreateRecordRequest()
+	request.Domain = common.StringPtr(zoneName)
+	request.DomainId = common.Uint64Ptr(zoneID)
+	request.SubDomain = common.StringPtr(recordName)
+	request.RecordType = common.StringPtr("TXT")
+	request.RecordLine = common.StringPtr("默认")
+	request.Value = common.StringPtr(value)
+	request.TTL = common.Uint64Ptr(uint64(d.config.TTL))
+
+	_, err = d.client.CreateRecord(request)
+	if err != nil {
+		return fmt.Errorf("dnspod: API call failed: %w", err)
 	}
 
 	return nil
@@ -105,19 +132,23 @@ func (d *DNSProvider) Present(domain, token, keyAuth string) error {
 // CleanUp removes the TXT record matching the specified parameters.
 func (d *DNSProvider) CleanUp(domain, token, keyAuth string) error {
 	fqdn, _ := dns01.GetRecord(domain, keyAuth)
-
-	records, err := d.findTxtRecords(domain, fqdn)
+	zoneID, zoneName, err := d.getHostedZone(domain)
 	if err != nil {
 		return err
 	}
 
-	zoneID, _, err := d.getHostedZone(domain)
+	records, err := d.findTxtRecords(zoneID, zoneName, fqdn)
 	if err != nil {
 		return err
 	}
 
-	for _, rec := range records {
-		_, err := d.client.Records.Delete(zoneID, rec.ID)
+	for _, record := range records {
+		request := dnspod.NewDeleteRecordRequest()
+		request.Domain = common.StringPtr(zoneName)
+		request.DomainId = common.Uint64Ptr(zoneID)
+		request.RecordId = record.RecordId
+
+		_, err := d.client.DeleteRecord(request)
 		if err != nil {
 			return err
 		}
@@ -131,70 +162,71 @@ func (d *DNSProvider) Timeout() (timeout, interval time.Duration) {
 	return d.config.PropagationTimeout, d.config.PollingInterval
 }
 
-func (d *DNSProvider) getHostedZone(domain string) (string, string, error) {
-	zones, _, err := d.client.Domains.List()
-	if err != nil {
-		return "", "", fmt.Errorf("API call failed: %w", err)
+func (d *DNSProvider) getHostedZone(domain string) (uint64, string, error) {
+	request := dnspod.NewDescribeDomainListRequest()
+
+	var domains []*dnspod.DomainListItem
+
+	for {
+		request.Offset = common.Int64Ptr(int64(len(domains)))
+		response, err := d.client.DescribeDomainList(request)
+		if err != nil {
+			return 0, "", fmt.Errorf("dnspod: API call failed: %w", err)
+		}
+
+		domains = append(domains, response.Response.DomainList...)
+
+		if uint64(len(domains)) >= *response.Response.DomainCountInfo.AllTotal {
+			break
+		}
 	}
 
 	authZone, err := dns01.FindZoneByFqdn(dns01.ToFqdn(domain))
 	if err != nil {
-		return "", "", err
+		return 0, "", err
 	}
 
-	var hostedZone dnspod.Domain
-	for _, zone := range zones {
-		if zone.Name == dns01.UnFqdn(authZone) {
+	var hostedZone *dnspod.DomainListItem
+	for _, zone := range domains {
+		if *zone.Name == dns01.UnFqdn(authZone) {
 			hostedZone = zone
 		}
 	}
 
-	if hostedZone.ID == "" || hostedZone.ID == "0" {
-		return "", "", fmt.Errorf("zone %s not found in dnspod for domain %s", authZone, domain)
+	if hostedZone == nil {
+		return 0, "", fmt.Errorf("zone %s not found in dnspod for domain %s", authZone, domain)
 	}
 
-	return hostedZone.ID.String(), hostedZone.Name, nil
+	return *hostedZone.DomainId, *hostedZone.Name, nil
 }
 
-func (d *DNSProvider) newTxtRecord(zone, fqdn, value string, ttl int) *dnspod.Record {
-	name := extractRecordName(fqdn, zone)
-
-	return &dnspod.Record{
-		Type:  "TXT",
-		Name:  name,
-		Value: value,
-		Line:  "默认",
-		TTL:   strconv.Itoa(ttl),
-	}
-}
-
-func (d *DNSProvider) findTxtRecords(domain, fqdn string) ([]dnspod.Record, error) {
-	zoneID, zoneName, err := d.getHostedZone(domain)
+func (d *DNSProvider) findTxtRecords(zoneID uint64, zoneName, fqdn string) ([]*dnspod.RecordListItem, error) {
+	recordName, err := extractRecordName(fqdn, zoneName)
 	if err != nil {
 		return nil, err
 	}
-
-	recordName := extractRecordName(fqdn, zoneName)
-
-	var records []dnspod.Record
-	result, _, err := d.client.Records.List(zoneID, recordName)
+	request := dnspod.NewDescribeRecordListRequest()
+	request.Domain = common.StringPtr(zoneName)
+	request.DomainId = common.Uint64Ptr(zoneID)
+	request.Subdomain = common.StringPtr(recordName)
+	request.RecordType = common.StringPtr("TXT")
+	request.RecordLine = common.StringPtr("默认")
+	response, err := d.client.DescribeRecordList(request)
 	if err != nil {
-		return records, fmt.Errorf("API call has failed: %w", err)
+		return nil, err
 	}
-
-	for _, record := range result {
-		if record.Name == recordName {
-			records = append(records, record)
-		}
-	}
-
-	return records, nil
+	return response.Response.RecordList, nil
 }
 
-func extractRecordName(fqdn, zone string) string {
-	name := dns01.UnFqdn(fqdn)
-	if idx := strings.Index(name, "."+zone); idx != -1 {
-		return name[:idx]
+func extractRecordName(fqdn, zone string) (string, error) {
+	asciiDomain, err := idna.ToASCII(zone)
+	if err != nil {
+		return "", fmt.Errorf("fail to convert punycode: %w", err)
 	}
-	return name
+
+	name := dns01.UnFqdn(fqdn)
+	if idx := strings.Index(name, "."+asciiDomain); idx != -1 {
+		return name[:idx], nil
+	}
+	return name, nil
 }

d0198bb3a3845a14964b4492e2d3f90ad610e0d9
Fernandez Ludovic <ldez@users.noreply.github.com>
2023-07-23 18:40:02 +0200

    Add DNS provider for hosting.nl

--- providers/dns/zz_gen_dns_providers.go
+++ providers/dns/zz_gen_dns_providers.go
@@ -60,6 +60,7 @@ import (
 	"github.com/go-acme/lego/v4/providers/dns/googledomains"
 	"github.com/go-acme/lego/v4/providers/dns/hetzner"
 	"github.com/go-acme/lego/v4/providers/dns/hostingde"
+	"github.com/go-acme/lego/v4/providers/dns/hostingnl"
 	"github.com/go-acme/lego/v4/providers/dns/hosttech"
 	"github.com/go-acme/lego/v4/providers/dns/httpnet"
 	"github.com/go-acme/lego/v4/providers/dns/httpreq"
@@ -258,6 +259,8 @@ func NewDNSChallengeProviderByName(name string) (challenge.Provider, error) {
 		return hetzner.NewDNSProvider()
 	case "hostingde":
 		return hostingde.NewDNSProvider()
+	case "hostingnl":
+		return hostingnl.NewDNSProvider()
 	case "hosttech":
 		return hosttech.NewDNSProvider()
 	case "httpnet":

2d5df2df1e9459615324f5660062e66718b8591d
Fernandez Ludovic <ldez@users.noreply.github.com>
2023-07-27 17:30:18 +0200

    review: improve cleanning
    Co-authored-by: David King <king.c.david@googlemail.com>

--- providers/dns/route53/route53.go
+++ providers/dns/route53/route53.go
@@ -176,34 +176,39 @@ func (d *DNSProvider) CleanUp(domain, token, keyAuth string) error {
 		return fmt.Errorf("failed to determine Route 53 hosted zone ID: %w", err)
 	}
 
-	records, err := d.getExistingRecordSets(ctx, hostedZoneID, info.EffectiveFQDN)
+	existingRecords, err := d.getExistingRecordSets(ctx, hostedZoneID, info.EffectiveFQDN)
 	if err != nil {
 		return fmt.Errorf("route53: %w", err)
 	}
 
-	if len(records) == 0 {
+	if len(existingRecords) == 0 {
 		return nil
 	}
 
-	var toDelete []awstypes.ResourceRecord
-	for _, record := range records {
-		if deref(record.Value) == `"`+info.Value+`"` {
-			toDelete = append(toDelete, record)
+	var nonLegoRecords []awstypes.ResourceRecord
+	for _, record := range existingRecords {
+		if deref(record.Value) != `"`+info.Value+`"` {
+			nonLegoRecords = append(nonLegoRecords, record)
 		}
 	}
 
-	if len(toDelete) == 0 {
-		return nil
-	}
+	action := awstypes.ChangeActionUpsert
 
 	recordSet := &awstypes.ResourceRecordSet{
 		Name:            aws.String(info.EffectiveFQDN),
 		Type:            "TXT",
 		TTL:             aws.Int64(int64(d.config.TTL)),
-		ResourceRecords: toDelete,
+		ResourceRecords: nonLegoRecords,
 	}
 
-	err = d.changeRecord(ctx, awstypes.ChangeActionDelete, hostedZoneID, recordSet)
+	// If the records are only records created by lego.
+	if len(nonLegoRecords) == 0 {
+		action = awstypes.ChangeActionDelete
+
+		recordSet.ResourceRecords = existingRecords
+	}
+
+	err = d.changeRecord(ctx, action, hostedZoneID, recordSet)
 	if err != nil {
 		return fmt.Errorf("route53: %w", err)
 	}

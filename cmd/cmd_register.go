7125e6fa4c71217fce7695f960cad004f4271ebf
schou <pschou@users.noreply.github.com>
2023-12-06 09:42:46 -0500

    remove domain check for register
    Signed-off-by: schou <pschou@users.noreply.github.com>

--- cmd/cmd_register.go
+++ cmd/cmd_register.go
@@ -12,15 +12,6 @@ func createRegister() *cli.Command {
 		Name:  "register",
 		Usage: "Register an account",
 		Before: func(ctx *cli.Context) error {
-			// we require either domains or csr, but not both
-			hasDomains := len(ctx.StringSlice("domains")) > 0
-			hasCsr := len(ctx.String("csr")) > 0
-			if hasDomains && hasCsr {
-				log.Fatal("Please specify either --domains/-d or --csr/-c, but not both")
-			}
-			if !hasDomains && !hasCsr {
-				log.Fatal("Please specify --domains/-d (or --csr/-c if you already have a CSR)")
-			}
 			return nil
 		},
 		Action: registerAction,

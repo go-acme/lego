0f098a581e63575dd6b5deac1da8634c602cb5fa
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-01-27 22:46:14 +0100

    chore: generate

--- cmd/zz_gen_cmd_dnshelp.go
+++ cmd/zz_gen_cmd_dnshelp.go
@@ -2078,7 +2078,7 @@ func displayDNSHelp(w io.Writer, name string) error {
 		ew.writeln()
 
 		ew.writeln(`Additional Configuration:`)
-		ew.writeln(`	- "PDNS_CUSTOM_API_VERSION":	Skip API version autodetection and use custom version number.`)
+		ew.writeln(`	- "PDNS_API_VERSION":	Skip API version autodetection and use the provided version number.`)
 		ew.writeln(`	- "PDNS_HTTP_TIMEOUT":	API request timeout`)
 		ew.writeln(`	- "PDNS_POLLING_INTERVAL":	Time between DNS propagation check`)
 		ew.writeln(`	- "PDNS_PROPAGATION_TIMEOUT":	Maximum waiting time for DNS propagation`)

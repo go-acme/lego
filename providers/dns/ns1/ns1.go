009c429947c068455a401a583bdecadd6e67a71d
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-01-28 01:38:14 +0100

    chore: update gopkg.in/ns1/ns1-go.v2

--- providers/dns/ns1/ns1.go
+++ providers/dns/ns1/ns1.go
@@ -97,7 +97,7 @@ func (d *DNSProvider) Present(domain, token, keyAuth string) error {
 	if errors.Is(err, rest.ErrRecordMissing) || record == nil {
 		log.Infof("Create a new record for [zone: %s, fqdn: %s, domain: %s]", zone.Zone, info.EffectiveFQDN, domain)
 
-		record = dns.NewRecord(zone.Zone, dns01.UnFqdn(info.EffectiveFQDN), "TXT")
+		record = dns.NewRecord(zone.Zone, dns01.UnFqdn(info.EffectiveFQDN), "TXT", nil, nil)
 		record.TTL = d.config.TTL
 		record.Answers = []*dns.Answer{{Rdata: []string{info.Value}}}
 
@@ -157,7 +157,7 @@ func (d *DNSProvider) getHostedZone(fqdn string) (*dns.Zone, error) {
 
 	authZone = dns01.UnFqdn(authZone)
 
-	zone, _, err := d.client.Zones.Get(authZone)
+	zone, _, err := d.client.Zones.Get(authZone, false)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get zone [authZone: %q, fqdn: %q]: %w", authZone, fqdn, err)
 	}

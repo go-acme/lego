79b978b0b15a77c70666a235815b37fe6a34acbd
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-08-26 23:42:37 +0200

    fix: inversion of propagation and polling interval default value

--- providers/dns/mittwald/mittwald.go
+++ providers/dns/mittwald/mittwald.go
@@ -37,8 +37,8 @@ type Config struct {
 // NewDefaultConfig returns a default configuration for the DNSProvider.
 func NewDefaultConfig() *Config {
 	return &Config{
-		PropagationTimeout: env.GetOrDefaultSecond(EnvPropagationTimeout, dns01.DefaultPropagationTimeout),
-		PollingInterval:    env.GetOrDefaultSecond(EnvPollingInterval, 3*time.Minute),
+		PropagationTimeout: env.GetOrDefaultSecond(EnvPropagationTimeout, 2*time.Minute),
+		PollingInterval:    env.GetOrDefaultSecond(EnvPollingInterval, dns01.DefaultPollingInterval),
 		HTTPClient: &http.Client{
 			Timeout: env.GetOrDefaultSecond(EnvHTTPTimeout, 30*time.Second),
 		},

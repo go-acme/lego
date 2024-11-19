b87c7eb83a7c2f7f57b7bedd10d9d62f9ff152cf
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-11-19 04:35:52 +0100

    review: minor changes

--- providers/dns/rainyun/rainyun.go
+++ providers/dns/rainyun/rainyun.go
@@ -99,17 +99,17 @@ func (d *DNSProvider) Present(domain, token, keyAuth string) error {
 
 	authZone, err := dns01.FindZoneByFqdn(info.EffectiveFQDN)
 	if err != nil {
-		return fmt.Errorf("epik: could not find zone for domain %q: %w", domain, err)
-	}
-
-	domainID, err := d.findDomainID(ctx, dns01.UnFqdn(authZone))
-	if err != nil {
-		return fmt.Errorf("rainyun: find domain ID: %w", err)
+		return fmt.Errorf("rainyun: could not find zone for domain %q: %w", domain, err)
 	}
 
 	subDomain, err := dns01.ExtractSubDomain(info.EffectiveFQDN, authZone)
 	if err != nil {
 		return fmt.Errorf("rainyun: %w", err)
+	}
+
+	domainID, err := d.findDomainID(ctx, dns01.UnFqdn(authZone))
+	if err != nil {
+		return fmt.Errorf("rainyun: find domain ID: %w", err)
 	}
 
 	record := internal.Record{
@@ -137,7 +137,7 @@ func (d *DNSProvider) CleanUp(domain, token, keyAuth string) error {
 
 	authZone, err := dns01.FindZoneByFqdn(info.EffectiveFQDN)
 	if err != nil {
-		return fmt.Errorf("epik: could not find zone for domain %q: %w", domain, err)
+		return fmt.Errorf("rainyun: could not find zone for domain %q: %w", domain, err)
 	}
 
 	domainID, err := d.findDomainID(ctx, dns01.UnFqdn(authZone))

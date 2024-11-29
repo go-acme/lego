37b2dc25fc42dfb83f9492c63aff3509db65c21a
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-11-29 17:00:43 +0100

    Add DNS provider for ManageEngine CloudDNS

---
+++ providers/dns/manageengine/manageengine.go
@@ -0,0 +1,189 @@
+// Package manageengine implements a DNS provider for solving the DNS-01 challenge using ManageEngine CloudDNS.
+package manageengine
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/go-acme/lego/v4/challenge/dns01"
+	"github.com/go-acme/lego/v4/platform/config/env"
+	"github.com/go-acme/lego/v4/providers/dns/manageengine/internal"
+)
+
+// Environment variables names.
+const (
+	envNamespace = "MANAGEENGINE_"
+
+	EnvClientID     = envNamespace + "CLIENT_ID"
+	EnvClientSecret = envNamespace + "CLIENT_SECRET"
+
+	EnvTTL                = envNamespace + "TTL"
+	EnvPropagationTimeout = envNamespace + "PROPAGATION_TIMEOUT"
+	EnvPollingInterval    = envNamespace + "POLLING_INTERVAL"
+)
+
+// Config is used to configure the creation of the DNSProvider.
+type Config struct {
+	ClientID     string
+	ClientSecret string
+
+	PropagationTimeout time.Duration
+	PollingInterval    time.Duration
+	TTL                int
+}
+
+// NewDefaultConfig returns a default configuration for the DNSProvider.
+func NewDefaultConfig() *Config {
+	return &Config{
+		TTL:                env.GetOrDefaultInt(EnvTTL, dns01.DefaultTTL),
+		PropagationTimeout: env.GetOrDefaultSecond(EnvPropagationTimeout, dns01.DefaultPropagationTimeout),
+		PollingInterval:    env.GetOrDefaultSecond(EnvPollingInterval, dns01.DefaultPollingInterval),
+	}
+}
+
+// DNSProvider implements the challenge.Provider interface.
+type DNSProvider struct {
+	config *Config
+	client *internal.Client
+}
+
+// NewDNSProvider returns a DNSProvider instance configured for ManageEngine CloudDNS.
+func NewDNSProvider() (*DNSProvider, error) {
+	values, err := env.Get(EnvClientID, EnvClientSecret)
+	if err != nil {
+		return nil, fmt.Errorf("manageengine: %w", err)
+	}
+
+	config := NewDefaultConfig()
+	config.ClientID = values[EnvClientID]
+	config.ClientSecret = values[EnvClientSecret]
+
+	return NewDNSProviderConfig(config)
+}
+
+// NewDNSProviderConfig return a DNSProvider instance configured for ManageEngine CloudDNS.
+func NewDNSProviderConfig(config *Config) (*DNSProvider, error) {
+	if config == nil {
+		return nil, errors.New("manageengine: the configuration of the DNS provider is nil")
+	}
+
+	if config.ClientID == "" || config.ClientSecret == "" {
+		return nil, errors.New("manageengine: credentials missing")
+	}
+
+	client := internal.NewClient(context.Background(), config.ClientID, config.ClientSecret)
+
+	return &DNSProvider{
+		config: config,
+		client: client,
+	}, nil
+}
+
+// Present creates a TXT record using the specified parameters.
+func (d *DNSProvider) Present(domain, token, keyAuth string) error {
+	ctx := context.Background()
+
+	info := dns01.GetChallengeInfo(domain, keyAuth)
+
+	authZone, err := dns01.FindZoneByFqdn(info.EffectiveFQDN)
+	if err != nil {
+		return fmt.Errorf("manageengine: could not find zone for domain %q: %w", domain, err)
+	}
+
+	zoneID, err := d.findZoneID(ctx, authZone)
+	if err != nil {
+		return fmt.Errorf("manageengine: find zone ID: %w", err)
+	}
+
+	record := internal.ZoneRecord{
+		ZoneID:     zoneID,
+		DomainName: info.EffectiveFQDN,
+		DomainTTL:  d.config.TTL,
+		RecordType: "TXT",
+		Records: []internal.Record{{
+			Value: []string{info.Value},
+		}},
+	}
+
+	err = d.client.CreateZoneRecord(ctx, zoneID, record)
+	if err != nil {
+		return fmt.Errorf("manageengine: create zone record: %w", err)
+	}
+
+	return nil
+}
+
+// CleanUp removes the TXT record matching the specified parameters.
+func (d *DNSProvider) CleanUp(domain, token, keyAuth string) error {
+	ctx := context.Background()
+
+	info := dns01.GetChallengeInfo(domain, keyAuth)
+
+	authZone, err := dns01.FindZoneByFqdn(info.EffectiveFQDN)
+	if err != nil {
+		return fmt.Errorf("manageengine: could not find zone for domain %q: %w", domain, err)
+	}
+
+	zoneID, err := d.findZoneID(ctx, authZone)
+	if err != nil {
+		return fmt.Errorf("manageengine: find zone ID: %w", err)
+	}
+
+	recordID, err := d.findRecordID(ctx, zoneID, info)
+	if err != nil {
+		return fmt.Errorf("manageengine: find record ID: %w", err)
+	}
+
+	err = d.client.DeleteZoneRecord(ctx, zoneID, recordID)
+	if err != nil {
+		return fmt.Errorf("manageengine: delete zone record: %w", err)
+	}
+
+	return nil
+}
+
+// Timeout returns the timeout and interval to use when checking for DNS propagation.
+// Adjusting here to cope with spikes in propagation times.
+func (d *DNSProvider) Timeout() (timeout, interval time.Duration) {
+	return d.config.PropagationTimeout, d.config.PollingInterval
+}
+
+func (d *DNSProvider) findZoneID(ctx context.Context, authZone string) (int, error) {
+	zones, err := d.client.GetAllZones(ctx)
+	if err != nil {
+		return 0, fmt.Errorf("get all zone groups: %w", err)
+	}
+
+	for _, zone := range zones {
+		if strings.EqualFold(zone.ZoneName, authZone) {
+			return zone.ZoneID, nil
+		}
+	}
+
+	return 0, fmt.Errorf(" zone not found %s", authZone)
+}
+
+func (d *DNSProvider) findRecordID(ctx context.Context, zoneID int, info dns01.ChallengeInfo) (int, error) {
+	zoneRecords, err := d.client.GetAllZoneRecords(ctx, zoneID)
+	if err != nil {
+		return 0, fmt.Errorf("manageengine: get all zone records: %w", err)
+	}
+
+	for _, zoneRecord := range zoneRecords {
+		if !strings.EqualFold(zoneRecord.DomainName, info.EffectiveFQDN) {
+			continue
+		}
+
+		for _, record := range zoneRecord.Records {
+			if slices.Contains(record.Value, info.Value) {
+				return record.ID, nil
+			}
+		}
+	}
+
+	return 0, fmt.Errorf("manageengine: record not found: zone ID: %d", zoneID)
+}
---
+++ providers/dns/manageengine/manageengine_test.go
@@ -0,0 +1,143 @@
+package manageengine
+
+import (
+	"testing"
+
+	"github.com/go-acme/lego/v4/platform/tester"
+	"github.com/stretchr/testify/require"
+)
+
+const envDomain = envNamespace + "DOMAIN"
+
+var envTest = tester.NewEnvTest(EnvClientID, EnvClientSecret).WithDomain(envDomain)
+
+func TestNewDNSProvider(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		envVars  map[string]string
+		expected string
+	}{
+		{
+			desc: "success",
+			envVars: map[string]string{
+				EnvClientID:     "abc",
+				EnvClientSecret: "secret",
+			},
+		},
+		{
+			desc: "missing client ID",
+			envVars: map[string]string{
+				EnvClientID:     "",
+				EnvClientSecret: "secret",
+			},
+			expected: "manageengine: some credentials information are missing: MANAGEENGINE_CLIENT_ID",
+		},
+		{
+			desc: "missing client secret",
+			envVars: map[string]string{
+				EnvClientID:     "abc",
+				EnvClientSecret: "",
+			},
+			expected: "manageengine: some credentials information are missing: MANAGEENGINE_CLIENT_SECRET",
+		},
+		{
+			desc:     "missing credentials",
+			envVars:  map[string]string{},
+			expected: "manageengine: some credentials information are missing: MANAGEENGINE_CLIENT_ID,MANAGEENGINE_CLIENT_SECRET",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			defer envTest.RestoreEnv()
+			envTest.ClearEnv()
+
+			envTest.Apply(test.envVars)
+
+			p, err := NewDNSProvider()
+
+			if test.expected == "" {
+				require.NoError(t, err)
+				require.NotNil(t, p)
+				require.NotNil(t, p.config)
+				require.NotNil(t, p.client)
+			} else {
+				require.EqualError(t, err, test.expected)
+			}
+		})
+	}
+}
+
+func TestNewDNSProviderConfig(t *testing.T) {
+	testCases := []struct {
+		desc         string
+		clientID     string
+		clientSecret string
+		expected     string
+	}{
+		{
+			desc:         "success",
+			clientID:     "abc",
+			clientSecret: "secret",
+		},
+		{
+			desc:         "missing client ID",
+			clientSecret: "secret",
+			expected:     "manageengine: credentials missing",
+		},
+		{
+			desc:     "missing client secret",
+			clientID: "abc",
+			expected: "manageengine: credentials missing",
+		},
+		{
+			desc:     "missing credentials",
+			expected: "manageengine: credentials missing",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			config := NewDefaultConfig()
+			config.ClientID = test.clientID
+			config.ClientSecret = test.clientSecret
+
+			p, err := NewDNSProviderConfig(config)
+
+			if test.expected == "" {
+				require.NoError(t, err)
+				require.NotNil(t, p)
+				require.NotNil(t, p.config)
+				require.NotNil(t, p.client)
+			} else {
+				require.EqualError(t, err, test.expected)
+			}
+		})
+	}
+}
+
+func TestLivePresent(t *testing.T) {
+	if !envTest.IsLiveTest() {
+		t.Skip("skipping live test")
+	}
+
+	envTest.RestoreEnv()
+	provider, err := NewDNSProvider()
+	require.NoError(t, err)
+
+	err = provider.Present(envTest.GetDomain(), "", "123d==")
+	require.NoError(t, err)
+}
+
+func TestLiveCleanUp(t *testing.T) {
+	if !envTest.IsLiveTest() {
+		t.Skip("skipping live test")
+	}
+
+	envTest.RestoreEnv()
+	provider, err := NewDNSProvider()
+	require.NoError(t, err)
+
+	err = provider.CleanUp(envTest.GetDomain(), "", "123d==")
+	require.NoError(t, err)
+}

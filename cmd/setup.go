6eca5c1b9311ac39accd842884134de602020a69
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-12-01 03:48:31 +0100

    fix(cli): use retryable client for ACME server calls

--- cmd/setup.go
+++ cmd/setup.go
@@ -14,6 +14,7 @@ import (
 	"github.com/go-acme/lego/v4/lego"
 	"github.com/go-acme/lego/v4/log"
 	"github.com/go-acme/lego/v4/registration"
+	"github.com/hashicorp/go-retryablehttp"
 	"github.com/urfave/cli/v2"
 )
 
@@ -55,6 +56,12 @@ func newClient(ctx *cli.Context, acc registration.User, keyType certcrypto.KeyTy
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		}
 	}
+
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = 5
+	retryClient.HTTPClient = config.HTTPClient
+
+	config.HTTPClient = retryClient.StandardClient()
 
 	client, err := lego.NewClient(config)
 	if err != nil {

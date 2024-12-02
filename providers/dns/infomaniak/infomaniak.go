fa00c8238556cfef03aaff0e5ca9b86af357dd71
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-12-02 16:05:26 +0100

    infomaniak: increase default propagation timeout

--- providers/dns/infomaniak/infomaniak.go
+++ providers/dns/infomaniak/infomaniak.go
@@ -47,9 +47,9 @@ type Config struct {
 func NewDefaultConfig() *Config {
 	return &Config{
 		APIEndpoint:        env.GetOrDefaultString(EnvEndpoint, internal.DefaultBaseURL),
-		TTL:                env.GetOrDefaultInt(EnvTTL, 7200),
-		PropagationTimeout: env.GetOrDefaultSecond(EnvPropagationTimeout, dns01.DefaultPropagationTimeout),
-		PollingInterval:    env.GetOrDefaultSecond(EnvPollingInterval, dns01.DefaultPollingInterval),
+		TTL:                env.GetOrDefaultInt(EnvTTL, 300),
+		PropagationTimeout: env.GetOrDefaultSecond(EnvPropagationTimeout, 2*time.Minute),
+		PollingInterval:    env.GetOrDefaultSecond(EnvPollingInterval, 10*time.Second),
 		HTTPClient: &http.Client{
 			Timeout: env.GetOrDefaultSecond(EnvHTTPTimeout, 30*time.Second),
 		},

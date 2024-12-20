258fb88ec9d716515180cb59247c1c8fdc49c970
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-12-20 15:11:10 +0100

    Prepare release v4.21.0

--- acme/api/internal/sender/useragent.go
+++ acme/api/internal/sender/useragent.go
@@ -4,10 +4,10 @@ package sender
 
 const (
 	// ourUserAgent is the User-Agent of this underlying library package.
-	ourUserAgent = "xenolf-acme/4.20.4"
+	ourUserAgent = "xenolf-acme/4.21.0"
 
 	// ourUserAgentComment is part of the UA comment linked to the version status of this underlying library package.
 	// values: detach|release
 	// NOTE: Update this with each tagged release.
-	ourUserAgentComment = "detach"
+	ourUserAgentComment = "release"
 )

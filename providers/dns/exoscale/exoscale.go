375300f969cf56133e10ebdbf33ffba831c8e9fe
mlec <42201667+mlec1@users.noreply.github.com>
2025-06-08 23:17:45 +0200

    exoscale: fix find record (#2535)
    Co-authored-by: Fernandez Ludovic <ldez@users.noreply.github.com>

--- providers/dns/exoscale/exoscale.go
+++ providers/dns/exoscale/exoscale.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	egoscale "github.com/exoscale/egoscale/v3"
@@ -215,7 +216,8 @@ func (d *DNSProvider) findExistingRecordID(zoneID egoscale.UUID, recordName stri
 	}
 
 	for _, record := range records.DNSDomainRecords {
-		if record.Name == recordName && record.Type == egoscale.DNSDomainRecordTypeTXT && record.Content == value {
+		if record.Name == recordName && record.Type == egoscale.DNSDomainRecordTypeTXT &&
+			(record.Content == value || record.Content == strconv.Quote(value)) {
 			return record.ID, nil
 		}
 	}

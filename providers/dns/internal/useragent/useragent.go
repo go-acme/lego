99a79f338c5d827d3f4a7c058eee532c1605448d
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-09-13 13:16:13 +0200

    Prepare release v4.26.0

--- providers/dns/internal/useragent/useragent.go
+++ providers/dns/internal/useragent/useragent.go
@@ -10,12 +10,12 @@ import (
 
 const (
 	// ourUserAgent is the User-Agent of this underlying library package.
-	ourUserAgent = "goacme-lego/4.25.2"
+	ourUserAgent = "goacme-lego/4.26.0"
 
 	// ourUserAgentComment is part of the UA comment linked to the version status of this underlying library package.
 	// values: detach|release
 	// NOTE: Update this with each tagged release.
-	ourUserAgentComment = "detach"
+	ourUserAgentComment = "release"
 )
 
 // Get builds and returns the User-Agent string.

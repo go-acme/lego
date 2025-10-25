732f07fe761bcc16dbf7f8bcf98ae7e781da8c28
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-10-25 18:00:40 +0200

    feat: debug transport

---
+++ providers/dns/internal/clientdebug/client.go
@@ -0,0 +1,52 @@
+package clientdebug
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"strconv"
+)
+
+type DumpTransport struct {
+	rt http.RoundTripper
+}
+
+func NewDumpTransport(rt http.RoundTripper) *DumpTransport {
+	if rt == nil {
+		rt = http.DefaultTransport
+	}
+
+	return &DumpTransport{rt: rt}
+}
+
+func (d *DumpTransport) RoundTrip(h *http.Request) (*http.Response, error) {
+	dump, _ := httputil.DumpRequestOut(h, true)
+
+	println("[HTTP Request]")
+	println(string(dump))
+
+	resp, err := d.rt.RoundTrip(h)
+
+	dump, _ = httputil.DumpResponse(resp, true)
+
+	println("[HTTP Response]")
+	println(string(dump))
+
+	return resp, err
+}
+
+// Wrap wraps an HTTP client Transport with the [DumpTransport].
+func Wrap(client *http.Client) *http.Client {
+	val, found := os.LookupEnv("LEGO_DEBUG_DNS_API_HTTP_CLIENT")
+	if !found {
+		return client
+	}
+
+	if ok, _ := strconv.ParseBool(val); !ok {
+		return client
+	}
+
+	client.Transport = NewDumpTransport(client.Transport)
+
+	return client
+}
---
+++ providers/dns/internal/clientdebug/client_test.go
@@ -0,0 +1,36 @@
+package clientdebug
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWrap_default(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+
+	client := Wrap(server.Client())
+
+	assert.Equal(t, server.Client(), client)
+
+	resp, err := client.Transport.RoundTrip(httptest.NewRequest(http.MethodGet, server.URL, nil))
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestWrap_enable(t *testing.T) {
+	t.Setenv("LEGO_DEBUG_DNS_API_HTTP_CLIENT", "true")
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+
+	client := Wrap(server.Client())
+
+	resp, err := client.Transport.RoundTrip(httptest.NewRequest(http.MethodGet, server.URL, nil))
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}

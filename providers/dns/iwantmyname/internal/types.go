053e78a09bd12c360ea05e757d652519341b12ca
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-10-29 03:04:12 +0100

    iwantmyname: provider deprecation

--- providers/dns/iwantmyname/internal/types.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-// Record represents a record.
-type Record struct {
-	Hostname string `url:"hostname,omitempty"`
-	Type     string `url:"type,omitempty"`
-	Value    string `url:"value,omitempty"`
-	TTL      int    `url:"ttl,omitempty"`
-}

ff885d99c2e45f166a0c9592ea047d8c938604d3
Ludovic Fernandez <ldez@users.noreply.github.com>
2025-12-23 16:39:06 +0100

    gandiv5: fix API Key header (#2769)

--- providers/dns/gandiv5/internal/client_test.go
+++ providers/dns/gandiv5/internal/client_test.go
@@ -9,23 +9,29 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func mockBuilder() *servermock.Builder[*Client] {
+func mockBuilder(apiKey, pat string) *servermock.Builder[*Client] {
+	checkHeaders := servermock.CheckHeader().WithJSONHeaders()
+
+	if apiKey != "" {
+		checkHeaders = checkHeaders.WithAuthorization("Apikey secret-apikey")
+	} else {
+		checkHeaders = checkHeaders.WithAuthorization("Bearer secret-pat")
+	}
+
 	return servermock.NewBuilder[*Client](
 		func(server *httptest.Server) (*Client, error) {
-			client := NewClient("secret", "xxx")
+			client := NewClient(apiKey, pat)
 			client.BaseURL, _ = url.Parse(server.URL)
 			client.HTTPClient = server.Client()
 
 			return client, nil
 		},
-		servermock.CheckHeader().WithJSONHeaders().
-			With("X-Api-Key", "secret").
-			WithAuthorization("Bearer xxx"),
+		checkHeaders,
 	)
 }
 
 func TestClient_AddTXTRecord(t *testing.T) {
-	client := mockBuilder().
+	client := mockBuilder("secret-apikey", "").
 		Route("GET /domains/example.com/records/foo/TXT",
 			servermock.ResponseFromFixture("add_txt_record_get.json")).
 		Route("PUT /domains/example.com/records/foo/TXT",
@@ -38,7 +44,7 @@ func TestClient_AddTXTRecord(t *testing.T) {
 }
 
 func TestClient_DeleteTXTRecord(t *testing.T) {
-	client := mockBuilder().
+	client := mockBuilder("", "secret-pat").
 		Route("DELETE /domains/example.com/records/foo/TXT",
 			servermock.ResponseFromFixture("api_response.json")).
 		Build(t)

8194277b77d033ddf7914b8639bb0ce4c751a822
Fernandez Ludovic <ldez@users.noreply.github.com>
2026-01-19 18:36:02 +0100

    chore: generate

--- cmd/zz_gen_cmd_dnshelp.go
+++ cmd/zz_gen_cmd_dnshelp.go
@@ -72,6 +72,7 @@ func allDNSCodes() string {
 		"edgeone",
 		"efficientip",
 		"epik",
+		"euserv",
 		"exec",
 		"exoscale",
 		"f5xc",
@@ -1514,6 +1515,28 @@ func displayDNSHelp(w io.Writer, name string) error {
 
 		ew.writeln()
 		ew.writeln(`More information: https://go-acme.github.io/lego/dns/epik`)
+
+	case "euserv":
+		// generated from: providers/dns/euserv/euserv.toml
+		ew.writeln(`Configuration for EUserv.`)
+		ew.writeln(`Code:	'euserv'`)
+		ew.writeln(`Since:	'v4.32.0'`)
+		ew.writeln()
+
+		ew.writeln(`Credentials:`)
+		ew.writeln(`	- "EUSERV_EMAIL":	The customer email address. You can also use the customer id instead.`)
+		ew.writeln(`	- "EUSERV_ORDER_ID":	The order ID of the API contract that you want to use for this login session.`)
+		ew.writeln(`	- "EUSERV_PASSWORD":	The customer account password.`)
+		ew.writeln()
+
+		ew.writeln(`Additional Configuration:`)
+		ew.writeln(`	- "EUSERV_HTTP_TIMEOUT":	API request timeout in seconds (Default: 30)`)
+		ew.writeln(`	- "EUSERV_POLLING_INTERVAL":	Time between DNS propagation check in seconds (Default: 2)`)
+		ew.writeln(`	- "EUSERV_PROPAGATION_TIMEOUT":	Maximum waiting time for DNS propagation in seconds (Default: 60)`)
+		ew.writeln(`	- "EUSERV_TTL":	The TTL of the TXT record used for the DNS challenge in seconds (Default: 120)`)
+
+		ew.writeln()
+		ew.writeln(`More information: https://go-acme.github.io/lego/dns/euserv`)
 
 	case "exec":
 		// generated from: providers/dns/exec/exec.toml

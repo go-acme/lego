bfce8e9e304303393ed1656b9487c05dbe4fe0d4
Ludovic Fernandez <ldez@users.noreply.github.com>
2026-03-27 20:06:15 +0100

    manual: add PropagationTimeout and PollingInterval options (#2943)

--- providers/dns/manual/manual.go
+++ providers/dns/manual/manual.go
@@ -3,26 +3,63 @@ package manual
 import (
 	"bufio"
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"time"
 
 	"github.com/go-acme/lego/v5/challenge"
 	"github.com/go-acme/lego/v5/challenge/dns01"
+	"github.com/go-acme/lego/v5/internal/env"
 )
 
 const (
 	dnsTemplate = `%s %d IN TXT %q`
 )
 
+// Environment variables names.
+const (
+	envNamespace = "MANUAL_"
+
+	EnvPropagationTimeout = envNamespace + "PROPAGATION_TIMEOUT"
+	EnvPollingInterval    = envNamespace + "POLLING_INTERVAL"
+)
+
 var _ challenge.Provider = (*DNSProvider)(nil)
 
+// Config is used to configure the creation of the DNSProvider.
+type Config struct {
+	PropagationTimeout time.Duration
+	PollingInterval    time.Duration
+}
+
+// NewDefaultConfig returns a default configuration for the DNSProvider.
+func NewDefaultConfig() *Config {
+	return &Config{
+		PropagationTimeout: env.GetOrDefaultSecond(EnvPropagationTimeout, dns01.DefaultPropagationTimeout),
+		PollingInterval:    env.GetOrDefaultSecond(EnvPollingInterval, dns01.DefaultPollingInterval),
+	}
+}
+
 // DNSProvider is an implementation of the ChallengeProvider interface.
-type DNSProvider struct{}
+type DNSProvider struct {
+	config *Config
+}
 
 // NewDNSProvider returns a DNSProvider instance.
 func NewDNSProvider() (*DNSProvider, error) {
-	return &DNSProvider{}, nil
+	config := NewDefaultConfig()
+
+	return NewDNSProviderConfig(config)
+}
+
+// NewDNSProviderConfig return a DNSProvider instance.
+func NewDNSProviderConfig(config *Config) (*DNSProvider, error) {
+	if config == nil {
+		return nil, errors.New("manual: the configuration of the DNS provider is nil")
+	}
+
+	return &DNSProvider{config: config}, nil
 }
 
 // Present prints instructions for manually creating the TXT record.
@@ -59,6 +96,12 @@ func (*DNSProvider) CleanUp(ctx context.Context, domain, token, keyAuth string) 
 	fmt.Printf(dnsTemplate+"\n", info.EffectiveFQDN, dns01.DefaultTTL, "...")
 
 	return nil
+}
+
+// Timeout returns the timeout and interval to use when checking for DNS propagation.
+// Adjusting here to cope with spikes in propagation times.
+func (d *DNSProvider) Timeout() (timeout, interval time.Duration) {
+	return d.config.PropagationTimeout, d.config.PollingInterval
 }
 
 // Sequential All DNS challenges for this provider will be resolved sequentially.
--- providers/dns/manual/manual_test.go
+++ providers/dns/manual/manual_test.go
@@ -4,11 +4,57 @@ import (
 	"io"
 	"os"
 	"testing"
+	"time"
 
+	"github.com/go-acme/lego/v5/internal/tester"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestDNSProviderManual(t *testing.T) {
+const envDomain = envNamespace + "DOMAIN"
+
+var envTest = tester.NewEnvTest(EnvPropagationTimeout, EnvPollingInterval).WithDomain(envDomain)
+
+func TestNewDNSProvider(t *testing.T) {
+	defer envTest.RestoreEnv()
+
+	envTest.ClearEnv()
+
+	envTest.Apply(map[string]string{
+		EnvPropagationTimeout: "12",
+		EnvPollingInterval:    "34",
+	})
+
+	p, err := NewDNSProvider()
+
+	require.NoError(t, err)
+	require.NotNil(t, p)
+	require.NotNil(t, p.config)
+
+	timeout, interval := p.Timeout()
+
+	assert.Equal(t, 12*time.Second, timeout)
+	assert.Equal(t, 34*time.Second, interval)
+}
+
+func TestNewDNSProviderConfig(t *testing.T) {
+	config := NewDefaultConfig()
+	config.PropagationTimeout = 12 * time.Second
+	config.PollingInterval = 34 * time.Second
+
+	p, err := NewDNSProviderConfig(config)
+
+	require.NoError(t, err)
+	require.NotNil(t, p)
+	require.NotNil(t, p.config)
+
+	timeout, interval := p.Timeout()
+
+	assert.Equal(t, 12*time.Second, timeout)
+	assert.Equal(t, 34*time.Second, interval)
+}
+
+func TestDNSProvider_manual(t *testing.T) {
 	backupStdin := os.Stdin
 
 	defer func() { os.Stdin = backupStdin }()

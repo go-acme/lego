fb7acd393174a182f09f3ef81c0d37396c55e290
Fernandez Ludovic <ldez@users.noreply.github.com>
2026-05-04 14:18:54 +0200

    Add DNS provider for NGENIX

---
+++ providers/dns/ngenix/internal/client_test.go
@@ -0,0 +1,96 @@
+package internal
+
+import (
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/go-acme/lego/v5/internal/tester/servermock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func mockBuilder() *servermock.Builder[*Client] {
+	return servermock.NewBuilder[*Client](
+		func(server *httptest.Server) (*Client, error) {
+			client, err := NewClient("user", "secret", "42")
+			if err != nil {
+				return nil, err
+			}
+
+			client.BaseURL, _ = url.Parse(server.URL)
+			client.HTTPClient = server.Client()
+
+			return client, nil
+		},
+		servermock.CheckHeader().
+			WithJSONHeaders().
+			WithBasicAuth("user/token", "secret"),
+	)
+}
+
+func TestClient_ListDNSZones(t *testing.T) {
+	client := mockBuilder().
+		Route("GET /dns-zone",
+			servermock.ResponseFromFixture("list_dns_zones.json"),
+			servermock.CheckQueryParameter().Strict().
+				With("customerId", "42"),
+		).
+		Build(t)
+
+	zones, err := client.ListDNSZones(t.Context())
+	require.NoError(t, err)
+
+	expected := []DNSZoneCollectionView{
+		{ID: 1, Name: "example.com"},
+		{ID: 2, Name: "example.org"},
+	}
+
+	assert.Equal(t, expected, zones)
+}
+
+func TestClient_GetDNSZone(t *testing.T) {
+	client := mockBuilder().
+		Route("GET /dns-zone/1",
+			servermock.ResponseFromFixture("get_dns_zone.json"),
+		).
+		Build(t)
+
+	zone, err := client.GetDNSZone(t.Context(), 1)
+	require.NoError(t, err)
+
+	expected := &DNSZone{
+		ID:   1,
+		Name: "example.com",
+		Records: []DNSRecord{
+			{Name: "www", Type: "A", Data: "198.51.100.1"},
+			{Name: "mail", Type: "MX", Data: "10 mail.example.com."},
+		},
+	}
+
+	assert.Equal(t, expected, zone)
+}
+
+func TestClient_UpdateDNSZone(t *testing.T) {
+	client := mockBuilder().
+		Route("PATCH /dns-zone/1",
+			servermock.ResponseFromFixture("update_dns_zone.json"),
+			servermock.CheckRequestJSONBodyFromFixture("update_dns_zone-request_add.json"),
+		).
+		Build(t)
+
+	records := []DNSRecord{
+		{Name: "www", Type: "A", Data: "198.51.100.1"},
+		{Name: "mail", Type: "MX", Data: "10 mail.example.com."},
+		{
+			Name: "_acme-challenge",
+			Type: "TXT",
+			Data: "ADw2sEd82DUgXcQ9hNBZThJs7zVJkR5v9JeSbAb9mZY",
+		},
+	}
+
+	updated, err := client.UpdateDNSZone(t.Context(), 1, DNSZoneUpdate{Records: records})
+	require.NoError(t, err)
+
+	assert.Equal(t, records, updated.Records)
+}
---
+++ providers/dns/ngenix/ngenix.go
@@ -0,0 +1,190 @@
+// Package ngenix implements a DNS provider for solving the DNS-01 challenge using Ngenix.
+package ngenix
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-acme/lego/v5/challenge"
+	"github.com/go-acme/lego/v5/challenge/dns01"
+	"github.com/go-acme/lego/v5/platform/env"
+	"github.com/go-acme/lego/v5/providers/dns/internal/clientdebug"
+	"github.com/go-acme/lego/v5/providers/dns/ngenix/internal"
+)
+
+// Environment variables names.
+const (
+	envNamespace = "NGENIX_"
+
+	EnvUsername   = envNamespace + "USERNAME"
+	EnvPassword   = envNamespace + "PASSWORD"
+	EnvCustomerID = envNamespace + "CUSTOMER_ID"
+
+	EnvPropagationTimeout = envNamespace + "PROPAGATION_TIMEOUT"
+	EnvPollingInterval    = envNamespace + "POLLING_INTERVAL"
+	EnvHTTPTimeout        = envNamespace + "HTTP_TIMEOUT"
+)
+
+var _ challenge.ProviderTimeout = (*DNSProvider)(nil)
+
+// Config is used to configure the creation of the DNSProvider.
+type Config struct {
+	Username   string
+	Password   string
+	CustomerID string
+
+	PropagationTimeout time.Duration
+	PollingInterval    time.Duration
+	HTTPClient         *http.Client
+}
+
+// NewDefaultConfig returns a default configuration for the DNSProvider.
+func NewDefaultConfig() *Config {
+	return &Config{
+		PropagationTimeout: env.GetOrDefaultSecond(EnvPropagationTimeout, dns01.DefaultPropagationTimeout),
+		PollingInterval:    env.GetOrDefaultSecond(EnvPollingInterval, dns01.DefaultPollingInterval),
+		HTTPClient: &http.Client{
+			Timeout: env.GetOrDefaultSecond(EnvHTTPTimeout, 30*time.Second),
+		},
+	}
+}
+
+// DNSProvider implements the challenge.Provider interface.
+type DNSProvider struct {
+	config *Config
+	client *internal.Client
+}
+
+// NewDNSProvider returns a DNSProvider instance configured for Ngenix.
+func NewDNSProvider() (*DNSProvider, error) {
+	values, err := env.Get(EnvUsername, EnvPassword, EnvCustomerID)
+	if err != nil {
+		return nil, fmt.Errorf("ngenix: %w", err)
+	}
+
+	config := NewDefaultConfig()
+	config.Username = values[EnvUsername]
+	config.Password = values[EnvPassword]
+	config.CustomerID = values[EnvCustomerID]
+
+	return NewDNSProviderConfig(config)
+}
+
+// NewDNSProviderConfig return a DNSProvider instance configured for Ngenix.
+func NewDNSProviderConfig(config *Config) (*DNSProvider, error) {
+	if config == nil {
+		return nil, errors.New("ngenix: the configuration of the DNS provider is nil")
+	}
+
+	client, err := internal.NewClient(config.Username, config.Password, config.CustomerID)
+	if err != nil {
+		return nil, fmt.Errorf("ngenix: %w", err)
+	}
+
+	if config.HTTPClient != nil {
+		client.HTTPClient = config.HTTPClient
+	}
+
+	client.HTTPClient = clientdebug.Wrap(client.HTTPClient)
+
+	return &DNSProvider{
+		config: config,
+		client: client,
+	}, nil
+}
+
+// Present creates a TXT record using the specified parameters.
+func (d *DNSProvider) Present(ctx context.Context, domain, token, keyAuth string) error {
+	info := dns01.GetChallengeInfo(ctx, domain, keyAuth)
+
+	zoneView, err := d.findZone(ctx, info.EffectiveFQDN)
+	if err != nil {
+		return fmt.Errorf("ngenix: %w", err)
+	}
+
+	zone, err := d.client.GetDNSZone(ctx, zoneView.ID)
+	if err != nil {
+		return fmt.Errorf("ngenix: get DNS zone: %w", err)
+	}
+
+	subDomain, err := dns01.ExtractSubDomain(info.EffectiveFQDN, zoneView.Name)
+	if err != nil {
+		return fmt.Errorf("ngenix: %w", err)
+	}
+
+	records := append(zone.Records, internal.DNSRecord{
+		Name: subDomain,
+		Type: "TXT",
+		Data: info.Value,
+	})
+
+	_, err = d.client.UpdateDNSZone(ctx, zone.ID, internal.DNSZoneUpdate{Records: records})
+	if err != nil {
+		return fmt.Errorf("ngenix: update DNS zone (add): %w", err)
+	}
+
+	return nil
+}
+
+// CleanUp removes the TXT record matching the specified parameters.
+func (d *DNSProvider) CleanUp(ctx context.Context, domain, token, keyAuth string) error {
+	info := dns01.GetChallengeInfo(ctx, domain, keyAuth)
+
+	zoneView, err := d.findZone(ctx, info.EffectiveFQDN)
+	if err != nil {
+		return fmt.Errorf("ngenix: %w", err)
+	}
+
+	zone, err := d.client.GetDNSZone(ctx, zoneView.ID)
+	if err != nil {
+		return fmt.Errorf("ngenix: get DNS zone: %w", err)
+	}
+
+	subDomain, err := dns01.ExtractSubDomain(info.EffectiveFQDN, zoneView.Name)
+	if err != nil {
+		return fmt.Errorf("ngenix: %w", err)
+	}
+
+	var records []internal.DNSRecord
+
+	for _, record := range zone.Records {
+		if record.Type == "TXT" && record.Name == subDomain && record.Data == info.Value {
+			continue
+		}
+
+		records = append(records, record)
+	}
+
+	_, err = d.client.UpdateDNSZone(ctx, zone.ID, internal.DNSZoneUpdate{Records: records})
+	if err != nil {
+		return fmt.Errorf("ngenix: update DNS zone (remove): %w", err)
+	}
+
+	return nil
+}
+
+// Timeout returns the timeout and interval to use when checking for DNS propagation.
+// Adjusting here to cope with spikes in propagation times.
+func (d *DNSProvider) Timeout() (timeout, interval time.Duration) {
+	return d.config.PropagationTimeout, d.config.PollingInterval
+}
+
+func (d *DNSProvider) findZone(ctx context.Context, fqdn string) (*internal.DNSZoneCollectionView, error) {
+	zones, err := d.client.ListDNSZones(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("list DNS zones: %w", err)
+	}
+
+	for dom := range dns01.UnFqdnDomainsSeq(fqdn) {
+		for _, zone := range zones {
+			if zone.Name == dom {
+				return &zone, nil
+			}
+		}
+	}
+
+	return nil, errors.New("could not find zone")
+}

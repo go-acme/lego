db6ddaba788b7c4e9385418155a2255fa63e38ac
Fernandez Ludovic <ldez@users.noreply.github.com>
2026-05-20 21:17:12 +0200

    simply: update API client implementation

--- providers/dns/simply/internal/types.go
+++ providers/dns/simply/internal/types.go
@@ -1,5 +1,13 @@
 package internal
 
+type APIError struct {
+	Message string `json:"error"`
+}
+
+func (e *APIError) Error() string {
+	return e.Message
+}
+
 // Record represents the content of a DNS record.
 type Record struct {
 	ID       int64  `json:"record_id,omitempty"`
@@ -10,25 +18,10 @@ type Record struct {
 	Priority int    `json:"priority,omitempty"`
 }
 
-type Response interface {
-	GetStatus() int
-	GetMessage() string
-}
-
 // apiResponse represents an API response.
 type apiResponse[S any, R any] struct {
-	Status  int    `json:"status"`
-	Message string `json:"message"`
-	Records S      `json:"records,omitempty"`
-	Record  R      `json:"record,omitempty"`
-}
-
-func (a apiResponse[S, R]) GetStatus() int {
-	return a.Status
-}
-
-func (a apiResponse[S, R]) GetMessage() string {
-	return a.Message
+	Records S `json:"records,omitempty"`
+	Record  R `json:"record,omitempty"`
 }
 
 type recordHeader struct {
--- providers/dns/simply/simply_test.go
+++ providers/dns/simply/simply_test.go
@@ -1,9 +1,12 @@
 package simply
 
 import (
+	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/go-acme/lego/v5/internal/tester"
+	"github.com/go-acme/lego/v5/internal/tester/servermock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -141,5 +144,52 @@ func TestLiveCleanUp(t *testing.T) {
 	require.NoError(t, err)
 
 	err = provider.CleanUp(t.Context(), envTest.GetDomain(), "", "123d==")
+	require.NoError(t, err)
+}
+
+func mockBuilder() *servermock.Builder[*DNSProvider] {
+	return servermock.NewBuilder(
+		func(server *httptest.Server) (*DNSProvider, error) {
+			config := NewDefaultConfig()
+			config.AccountName = "S000000"
+			config.APIKey = "secret"
+			config.HTTPClient = server.Client()
+
+			p, err := NewDNSProviderConfig(config)
+			if err != nil {
+				return nil, err
+			}
+
+			p.client.BaseURL, _ = url.Parse(server.URL)
+
+			return p, nil
+		},
+		servermock.CheckHeader().
+			WithJSONHeaders(),
+	)
+}
+
+func TestDNSProvider_Present(t *testing.T) {
+	provider := mockBuilder().
+		Route("POST /my/products/example.com/dns/records/",
+			servermock.ResponseFromInternal("add_record.json"),
+			servermock.CheckRequestJSONBodyFromInternal("add_record-request.json"),
+		).
+		Build(t)
+
+	err := provider.Present(t.Context(), "example.com", "abc", "123d==")
+	require.NoError(t, err)
+}
+
+func TestDNSProvider_CleanUp(t *testing.T) {
+	provider := mockBuilder().
+		Route("DELETE /my/products/example.com/dns/records/123456789/",
+			servermock.ResponseFromInternal("success.json"),
+		).
+		Build(t)
+
+	provider.recordIDs["abc"] = 123456789
+
+	err := provider.CleanUp(t.Context(), "example.com", "abc", "123d==")
 	require.NoError(t, err)
 }

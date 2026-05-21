f004b50201ae7ebe6c0290e33fb7bf5bc998745f
Fernandez Ludovic <ldez@users.noreply.github.com>
2026-05-21 13:25:48 +0200

    stackpath: provider deprecation

--- providers/dns/stackpath/stackpath.go
+++ providers/dns/stackpath/stackpath.go
@@ -5,14 +5,10 @@ package stackpath
 import (
 	"context"
 	"errors"
-	"fmt"
 	"time"
 
 	"github.com/go-acme/lego/v5/challenge"
 	"github.com/go-acme/lego/v5/challenge/dns01"
-	"github.com/go-acme/lego/v5/platform/env"
-	"github.com/go-acme/lego/v5/providers/dns/internal/clientdebug"
-	"github.com/go-acme/lego/v5/providers/dns/stackpath/internal"
 )
 
 // Environment variables names.
@@ -42,120 +38,38 @@ type Config struct {
 
 // NewDefaultConfig returns a default configuration for the DNSProvider.
 func NewDefaultConfig() *Config {
-	return &Config{
-		TTL:                env.GetOrDefaultInt(EnvTTL, dns01.DefaultTTL),
-		PropagationTimeout: env.GetOrDefaultSecond(EnvPropagationTimeout, dns01.DefaultPropagationTimeout),
-		PollingInterval:    env.GetOrDefaultSecond(EnvPollingInterval, dns01.DefaultPollingInterval),
-	}
+	return &Config{}
 }
 
 // DNSProvider implements the challenge.Provider interface.
-type DNSProvider struct {
-	config *Config
-	client *internal.Client
-}
+type DNSProvider struct{}
 
 // NewDNSProvider returns a DNSProvider instance configured for Stackpath.
-// Credentials must be passed in the environment variables:
-// STACKPATH_CLIENT_ID, STACKPATH_CLIENT_SECRET, and STACKPATH_STACK_ID.
+//
+// Deprecated: The Stackpath DNS provider shut down in 2024.
 func NewDNSProvider() (*DNSProvider, error) {
-	values, err := env.Get(EnvClientID, EnvClientSecret, EnvStackID)
-	if err != nil {
-		return nil, fmt.Errorf("stackpath: %w", err)
-	}
-
-	config := NewDefaultConfig()
-	config.ClientID = values[EnvClientID]
-	config.ClientSecret = values[EnvClientSecret]
-	config.StackID = values[EnvStackID]
-
-	return NewDNSProviderConfig(config)
+	return NewDNSProviderConfig(&Config{})
 }
 
 // NewDNSProviderConfig return a DNSProvider instance configured for Stackpath.
+//
+// Deprecated: The Stackpath DNS provider shut down in 2024.
 func NewDNSProviderConfig(config *Config) (*DNSProvider, error) {
-	if config == nil {
-		return nil, errors.New("stackpath: the configuration of the DNS provider is nil")
-	}
-
-	if config.ClientID == "" || config.ClientSecret == "" {
-		return nil, errors.New("stackpath: credentials missing")
-	}
-
-	if config.StackID == "" {
-		return nil, errors.New("stackpath: stack id missing")
-	}
-
-	return &DNSProvider{
-		config: config,
-		client: internal.NewClient(config.StackID,
-			clientdebug.Wrap(
-				internal.CreateOAuthClient(context.Background(), config.ClientID, config.ClientSecret),
-			),
-		),
-	}, nil
+	return nil, errors.New("stackpath: provider has shut down")
 }
 
 // Present creates a TXT record to fulfill the dns-01 challenge.
 func (d *DNSProvider) Present(ctx context.Context, domain, token, keyAuth string) error {
-	info := dns01.GetChallengeInfo(ctx, domain, keyAuth)
-
-	zone, err := d.client.GetZones(ctx, info.EffectiveFQDN)
-	if err != nil {
-		return fmt.Errorf("stackpath: get zones: %w", err)
-	}
-
-	subDomain, err := dns01.ExtractSubDomain(info.EffectiveFQDN, zone.Domain)
-	if err != nil {
-		return fmt.Errorf("stackpath: %w", err)
-	}
-
-	record := internal.Record{
-		Name: subDomain,
-		Type: "TXT",
-		TTL:  d.config.TTL,
-		Data: info.Value,
-	}
-
-	err = d.client.CreateZoneRecord(ctx, zone, record)
-	if err != nil {
-		return fmt.Errorf("stackpath: create zone record: %w", err)
-	}
-
 	return nil
 }
 
 // CleanUp removes the TXT record matching the specified parameters.
 func (d *DNSProvider) CleanUp(ctx context.Context, domain, token, keyAuth string) error {
-	info := dns01.GetChallengeInfo(ctx, domain, keyAuth)
-
-	zone, err := d.client.GetZones(ctx, info.EffectiveFQDN)
-	if err != nil {
-		return fmt.Errorf("stackpath: get zones: %w", err)
-	}
-
-	subDomain, err := dns01.ExtractSubDomain(info.EffectiveFQDN, zone.Domain)
-	if err != nil {
-		return fmt.Errorf("stackpath: %w", err)
-	}
-
-	records, err := d.client.GetZoneRecords(ctx, subDomain, zone)
-	if err != nil {
-		return fmt.Errorf("stackpath: get zone records: %w", err)
-	}
-
-	for _, record := range records {
-		err = d.client.DeleteZoneRecord(ctx, zone, record)
-		if err != nil {
-			return fmt.Errorf("stackpath: delete zone record: %w", err)
-		}
-	}
-
 	return nil
 }
 
 // Timeout returns the timeout and interval to use when checking for DNS propagation.
 // Adjusting here to cope with spikes in propagation times.
 func (d *DNSProvider) Timeout() (timeout, interval time.Duration) {
-	return d.config.PropagationTimeout, d.config.PollingInterval
+	return dns01.DefaultPropagationTimeout, dns01.DefaultPollingInterval
 }

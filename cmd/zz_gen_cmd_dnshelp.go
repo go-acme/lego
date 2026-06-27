9d38a00ec696d23e3a8fcf3a90c14c023ba211a4
Fernandez Ludovic <ldez@users.noreply.github.com>
2026-06-27 22:09:49 +0200

    chore: generate

--- cmd/zz_gen_cmd_dnshelp.go
+++ cmd/zz_gen_cmd_dnshelp.go
@@ -1772,8 +1772,8 @@ func displayDNSHelp(w io.Writer, name string) error {
 
 		ew.writeln(`Additional Configuration:`)
 		ew.writeln(`	- "EUSERV_HTTP_TIMEOUT":	API request timeout in seconds (Default: 30)`)
-		ew.writeln(`	- "EUSERV_POLLING_INTERVAL":	Time between DNS propagation check in seconds (Default: 2)`)
-		ew.writeln(`	- "EUSERV_PROPAGATION_TIMEOUT":	Maximum waiting time for DNS propagation in seconds (Default: 60)`)
+		ew.writeln(`	- "EUSERV_POLLING_INTERVAL":	Time between DNS propagation check in seconds (Default: 10)`)
+		ew.writeln(`	- "EUSERV_PROPAGATION_TIMEOUT":	Maximum waiting time for DNS propagation in seconds (Default: 360)`)
 		ew.writeln(`	- "EUSERV_TTL":	The TTL of the TXT record used for the DNS challenge in seconds (Default: 120)`)
 
 		ew.writeln()
